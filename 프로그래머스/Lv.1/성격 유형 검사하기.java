bb1a8676962492ce89846eba47a5120ae058b598
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2023-11-28 17:21:31 +0900

    Create 성격 유형 검사하기.java

---
+++ 프로그래머스/Lv.1/성격 유형 검사하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < survey.length; i++){
+            char first = survey[i].charAt(0);
+            char second = survey[i].charAt(1);
+            if (choices[i] < 4) { // 1 ~ 3일떄 앞쪽 문자열의 정보 갱신
+                int idx = map.getOrDefault(first, 0);
+                map.put(first, idx += 4 - choices[i]);
+            }
+            
+            else if (choices[i] > 4) { // 5 ~ 7 일때 뒤쪽 문자열 정보 갱신
+                int idx = map.getOrDefault(second, 0);
+                map.put(second, idx += choices[i] - 4);
+            }
+        }
+        
+        
+        answer += (map.getOrDefault('R', 0) >= map.getOrDefault('T', 0)) ? 'R' : 'T';
+        answer += (map.getOrDefault('C', 0) >= map.getOrDefault('F', 0)) ? 'C' : 'F';
+        answer += (map.getOrDefault('J', 0) >= map.getOrDefault('M', 0)) ? 'J' : 'M';
+        answer += (map.getOrDefault('A', 0) >= map.getOrDefault('N', 0)) ? 'A' : 'N';
+        
+        for(Character s : map.keySet()){
+            System.out.println(s);
+        }
+        
+        // choices 1 ~ 3은 survey 앞 글자의 점수를 3 ~ 1점 추가, 5 ~ 7은 반대.
+        return answer;
+    }
+}
