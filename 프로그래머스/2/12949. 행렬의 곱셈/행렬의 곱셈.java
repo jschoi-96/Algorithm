44b4efc1307f5b82f9ce1789c5456a3642b394ea
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-04-16 15:55:33 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.80 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) { 
+        int len1 = arr1.length;
+        int len2 = arr2[0].length;
+        int[][] answer = new int[len1][len2];
+        
+        for(int i = 0; i < len1; i++){
+            for(int j = 0; j < len2; j++){
+                answer[i][j] = 0;
+                for(int k = 0; k < arr2.length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
