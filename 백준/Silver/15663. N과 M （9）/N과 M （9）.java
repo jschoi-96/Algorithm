59b8dae9662b53b3f9717ded4d6c7ee714c15066
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-04-24 01:47:47 +0900

    [Silver II] Title: N과 M (9), Time: 680 ms, Memory: 43092 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int m, n;
+    static int [] arr;
+    static int [] tmp;
+    static boolean[] visited = new boolean[10];
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> set = new LinkedHashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        tmp = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        recur(0);
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+
+    private static void recur(int depth) {
+        if (depth == m) {
+            String res = "";
+            for(int i = 0; i < depth; i++){
+               res += tmp[i] + " ";
+            }
+            set.add(res);
+            return;
+        }
+
+        for(int i = 0; i < n; i++){
+            if (!visited[i]){
+                visited[i] = true;
+                tmp[depth] = arr[i];
+                recur(depth + 1);
+                visited[i] = false;
+            }
+
+        }
+    }
+}
