304350248269182b082e55a5b43c12fc4ba3dbd9
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-05-14 18:06:38 +0900

    [level 2] Title: H-Index, Time: 0.55 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,17 +1,23 @@
 import java.util.*;
 class Solution {
-    // 1. 배열을 정렬을 한다.
-    // 2. 배열의 뒤에서 부터 순회를 하면서 
+    // h의 최대값 -> 뒤에서 부터 탐색해야함 
+    // 최초로 찾는 놈이 최대
     public int solution(int[] citations) {
+        int answer = 0;
         Arrays.sort(citations);
-        for(int h = citations.length; h >= 1; h--) {
-            if (isValid(citations, h)) return h;
-        } 
-        return 0;
-    }
-    
-    public boolean isValid(int [] citations, int h) {
-        int idx = citations.length - h;
-        return citations[idx] >= h;
+        // idx: 0 -> 1 -> 2..
+        for(int i = citations.length; i >= 1; i--) {
+            int idx = citations.length - i;
+            if (citations[idx] >= i) {
+                //System.out.println(citations[idx]);
+                return i;
+            }
+        }
+        // 0,1,3,5,6 
+        // 6번 이상 인용 -> 1편
+        // 5번 이상 인용 -> 2편
+        // 3번 이상 인용 -> 3편 (최댓값)
+        // 1번 이상 인용 -> 4편
+        return answer;
     }
 }
