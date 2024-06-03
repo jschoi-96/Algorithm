640403cdb9efdf68d770a3deb9b1e464480ab5e7
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-06-04 00:05:30 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,23 @@
+class Solution {
+    // current라는 현재값을
+    static boolean [] visited;
+    static int ans = 0;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0, 0);
+        return ans;
+    }
+    
+    public void dfs(int k, int[][] dungeons, int depth, int count) {
+        //System.out.println(count);
+        ans = Math.max(ans, count);
+        
+        for(int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], dungeons, depth + 1, count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
