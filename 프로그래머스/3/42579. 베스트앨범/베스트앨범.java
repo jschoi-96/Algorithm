173e136a1814681d421313c0baaf1a9a427d7edf
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-06-29 01:18:58 +0900

    [level 3] Title: 베스트앨범, Time: 0.91 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,39 +1,47 @@
 import java.util.*;
 class Solution {
-    // 1. 장르에 해당하는 플레이 타임을 맵에 넣는다. 
-    // 2. 플레이 타임이 더 많은 장르를 우선적으로 저장해야 한다.
-    // 2-1. 같은 장르 중에서 더 많이 재생된 노래를 우선적으로 저장
-    // 2-2. 재생 횟수가 같다면 고유 번호로 비교
     public int[] solution(String[] genres, int[] plays) {
-        List<Integer> answer = new ArrayList<>();
+        int[] answer = {};
+        // 1. 장르 수록
         Map<String, Integer> genreMap = new HashMap<>();
-        Map<Integer, Integer> indexMap = new HashMap<>();
         for(int i = 0; i < genres.length; i++) {
             genreMap.put(genres[i], genreMap.getOrDefault(genres[i], 0) + plays[i]);
-            indexMap.put(i, plays[i]);
         }
         
+        // 장르 재생 순서가 많은대로 내림차순
+        List<String> genreKey = new ArrayList<>(genreMap.keySet()); // keySet 기준으로 내림차순
+        genreKey.sort((a,b) -> genreMap.get(b) - genreMap.get(a));
         
-        // map을 내림차순해서 재생횟수가 많은 순서대로 정렬 (pop, classic)
-        List<String> keys = new ArrayList<>(genreMap.keySet());
-        Collections.sort(keys, (a,b) -> genreMap.get(b) - genreMap.get(a));
-     
-        // 고유 번호를 재생 횟수가 많은 순서대로 정렬
-        // [4, 2500], [3, 800] ... 
-        List<Integer> indexList = new ArrayList<>(indexMap.keySet());
-        Collections.sort(indexList, (a,b) -> indexMap.get(b) - indexMap.get(a)); 
-        for (String key : keys) {
-            int size = 0;
-            for(Integer index : indexList) {
-                if (genres[index].equals(key) && size < 2) {
-                    size++;
-                    answer.add(index);
+        
+        List<Integer> result = new ArrayList<>();
+        int idx = 0;
+        
+        for(String genre : genreKey) {
+            Map<Integer,Integer> playsMap = new HashMap<>();
+            for(int i = 0; i < genres.length; i++) {
+                if (genre.equals(genres[i])) {
+                    playsMap.put(i, plays[i]);
                 }
+            }
+            
+            List<Integer> playsList = new ArrayList<>(playsMap.keySet()); // index로 리스트를 만든 후에
+            playsList.sort((a,b) -> playsMap.get(b) - playsMap.get(a));
+            
+                        
+            if (playsList.size() == 1) {
+                result.add(playsList.get(0));
+            }
+            
+            else {
+                 result.add(playsList.get(0));
+                 result.add(playsList.get(1));
             }
         }
         
-        int [] res = new int[answer.size()];
-        for(int i = 0; i < res.length; i++) res[i] = answer.get(i);
-        return res;
+        int [] ans = new int [result.size()];
+        for(int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+        return ans;
     }
 }
