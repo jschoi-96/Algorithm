79ea9b2969c528d6a9d6e5cf6deebc3f195268f8
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-08-05 20:13:54 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14828 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,55 +1,52 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int n, m;
-    static int[][] board = new int[102][102];
-    static int[][] dist = new int[102][102];
-    static int[] dx = { 1, 0, -1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
-    static Queue<int[]> queue = new LinkedList<>();
-    static int res = 0, min_res = Integer.MAX_VALUE;
-
-    public static void main(String[] args) throws Exception {
-
+    static int [][] board, dist;
+    static boolean [][] visited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < n; i++) {
+        board = new int[n+2][m+2];
+        dist = new int[n+2][m+2];
+        visited = new boolean[n+2][m+2];
+        for(int i = 0; i < n; i++) {
             String input = br.readLine();
-            for (int j = 0; j < m; j++) {
+            for(int j = 0; j < m; j++) {
                 board[i][j] = input.charAt(j) - '0';
             }
         }
 
-        queue.add(new int[] { 0, 0 });
+        Queue<int []> q = new LinkedList<>();
+        q.add(new int[]{0,0});
         dist[0][0] = 1;
 
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
+        visited[0][0] = true;
+        while(!q.isEmpty()) {
+            int [] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
-
-            for (int dir = 0; dir < 4; dir++) {
+            for(int dir = 0; dir < 4; dir++) {
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-                    continue;
-                if (board[nx][ny] == 0 || dist[nx][ny] != 0)
-                    continue;
-
-                queue.add(new int[] { nx, ny });
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (board[nx][ny] == 0 || visited[nx][ny]) continue;
+                visited[nx][ny] = true;
                 dist[nx][ny] = dist[x][y] + 1;
-
+                q.add(new int[]{nx, ny});
             }
         }
-        System.out.println(dist[n - 1][m - 1]);
 
+        System.out.println(dist[n-1][m-1]);
     }
 }
