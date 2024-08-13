7503f3d5c76c35b773191d77c5fb6470311edb4f
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-08-13 20:51:35 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 720 ms, Memory: 66336 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,49 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
     static List<List<Integer>> graph = new ArrayList<>();
-    static boolean [] visited;
     static int [] parent;
+    static boolean [] visited;
+    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
+        visited = new boolean[n + 1];
+        parent = new int[n + 1];
         for(int i = 0; i <= n; i++){
             graph.add(new ArrayList<>());
         }
-        visited = new boolean[n + 1];
-        parent = new int[n+1];
-        for(int i = 0; i < n -1 ; i++) {
+
+        for(int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
-        bfs(1);
 
-        for(int i = 2; i < parent.length; i++){
-            System.out.println(parent[i]);
+        dfs(1);
+
+        StringBuilder sb = new StringBuilder();
+//        for(int i = 2; i <= n; i++){
+//            sb.append(graph.get(i).get(0)).append("\n");
+//        }
+
+        for(int i = 2; i <= n; i++) {
+            sb.append(parent[i]).append("\n");
         }
+        System.out.println(sb);
+
     }
 
-    public static void bfs(int start) {
+    public static void dfs(int start) {
         visited[start] = true;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            for(int next : graph.get(node)){
-                if (visited[next]) continue;
-                queue.add(next);
-                visited[next] = true;
-                parent[next] = node;
+        for(int nxt : graph.get(start)) {
+            if (!visited[nxt]) {
+                // System.out.println("start: " + start + " nxt: " + nxt);
+                parent[nxt] = start;
+                dfs(nxt);
             }
         }
     }
