9ce1863eb5386d6245ed52a6297e1bdc10635b82
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-08-19 15:43:19 +0900

    [Silver II] Title: 나무 자르기, Time: 840 ms, Memory: 119456 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -5,54 +5,39 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    /*
-        1. 자른 나무들의 총 합이 m이상이 되도록 높이를 설정해야 한다.
-        2. 높이의 최댓값을 구해서 리턴
-        3. 이분탐색의 시작 값은 최솟값, 끝 값은 나무의 최대 높이
-     */
     static int n, m;
-    static long [] arr;
+    static int [] trees;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        arr = new long[n + 1];
-
+        trees = new int[n];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            trees[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(trees);
 
-        long max_h = binarySearch(m);
-        System.out.println(max_h);
-    }
-
-    public static long binarySearch(int target) {
-        long st = arr[0];
-        long en = arr[arr.length - 1];
-
-        while(st < en) {
-            long mid = (st + en) / 2;
+        long lo = 0; long hi = trees[n-1];
+        while(lo <= hi) {
+            long mid = lo + (hi - lo) / 2;
 
             long res = 0;
-            for(int i = 0; i < arr.length; i++) {
-                if (arr[i] > mid) {
-                    res += arr[i] - mid;
-                }
+            for(int tree : trees) {
+                if (tree > mid) res += (tree - mid);
             }
 
-            if (res < target) {
-                en = mid;
+            if (res < m) {
+                hi = mid - 1;
             }
 
-            else {
-                st = mid + 1;
+            else { // res가 m보다 크다면 하한점을 높여줘서 res값을 줄여줘야 한다.
+                lo = mid + 1;
             }
         }
-        return en - 1;
+        System.out.println(lo - 1);
     }
 }
