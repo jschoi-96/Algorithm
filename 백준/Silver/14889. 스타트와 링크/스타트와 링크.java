011475cf175bd2a54b2675f61fb1b41ccc13dec6
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-09-25 20:42:12 +0900

    [Silver I] Title: 스타트와 링크, Time: 312 ms, Memory: 19508 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,62 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n;
     static int [][] board;
     static boolean [] visited;
-    static int min = Integer.MAX_VALUE;
+    static int res = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+
+        // [1,2], [3,4]
+        // [1,3], [2,4]
+        // [1,4], [2,3]
+
         board = new int[n][n];
         visited = new boolean[n];
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < n; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 재귀 종료 조건?
         dfs(0, 0);
-        System.out.println(min);
+        System.out.println(res);
     }
 
-    private static void dfs(int idx, int depth) {
-        if (depth == n / 2) {
-            calc();
+    public static void dfs(int idx, int cnt) {
+        if (cnt == n/2) {
+            int start = 0;
+            int link = 0;
+
+            for(int i = 0; i < n - 1; i++) {
+                for(int j = i + 1; j < n; j++) {
+                    if (visited[i] && visited[j]) {
+                        start += board[i][j] + board[j][i];
+                    }
+
+                    if (!visited[i] && !visited[j]) {
+                        link += board[i][j] + board[j][i];
+                    }
+                }
+            }
+
+            res = Math.min(res, Math.abs(start - link));
             return;
         }
 
         for(int i = idx; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(i + 1, depth + 1);
-                visited[i] = false;
+            if (!visited[idx]) {
+                visited[idx] = true;
+                dfs(i + 1, cnt + 1);
+                visited[idx] = false;
             }
         }
-    }
-
-    private static void calc() {
-        int start = 0;
-        int link = 0;
-        for(int i = 0; i < n - 1; i++){
-            for(int j = i + 1; j < n; j++) {
-                if (visited[i] && visited[j]) { // 두 곳을 모두 방문
-                    start += board[i][j];
-                    start += board[j][i];
-                }
-
-                else if (!visited[i] && !visited[j]) {
-                    link += board[i][j];
-                    link += board[j][i];
-                }
-            }
-        }
-        int res = Math.abs(start - link);
-        min = Math.min(res , min);
     }
 }
