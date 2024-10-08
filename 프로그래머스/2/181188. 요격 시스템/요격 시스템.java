55264e17cc8f947eda390e0a3724623bf33f7f9f
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-10-09 02:41:22 +0900

    [level 2] Title: 요격 시스템, Time: 414.39 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -4,14 +4,19 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(targets, (a,b) -> a[1] - b[1]);
+        
         int before = 0;
         for(int [] target : targets) {
-            
-            if (before > target[0]) continue; 
-            before = target[1]; 
-            System.out.println(before);
-            answer++;
+            // System.out.println(target[0] + " " + target[1]);
+            if (before <= target[0]) {
+                before = target[1];
+                answer++;
+            }
         }
+        
+        // [1,4]
+        // [3,7] [4,5] [4,8]
+        // [5,12] [10,14] [11,13]
         return answer;
     }
 }
