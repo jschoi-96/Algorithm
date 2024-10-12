c92253e5a91b32c8fab181cc0ef571947ea643af
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-10-13 01:52:02 +0900

    [Silver III] Title: N과 M (5), Time: 280 ms, Memory: 28456 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,14 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int m, n;
+    static int n, m;
     static int [] arr;
-    static int [] tmp = new int[10];
-    static boolean[] visited = new boolean[10000];
+    static int [] arr2;
+    static boolean[] visited = new boolean[10002];
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,33 +17,34 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         arr = new int[n];
+        arr2 = new int[n];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
 
-        recur(0);
-        System.out.println(sb.toString());
+
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
     }
 
-    private static void recur(int depth) {
+    public static void dfs(int depth) {
         if (depth == m) {
-            for(int i = 0; i < m; i++){
-                sb.append(tmp[i]).append(" ");
+            for(int i = 0; i < m; i++) {
+                sb.append(arr2[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             if (!visited[arr[i]]) {
+                arr2[depth] = arr[i];
                 visited[arr[i]] = true;
-                tmp[depth] = arr[i];
-                recur(depth + 1);
+                dfs(depth + 1);
                 visited[arr[i]] = false;
             }
         }
     }
-
 }
