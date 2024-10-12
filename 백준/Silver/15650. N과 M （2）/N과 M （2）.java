9c54cc9363910bca4ca4b6eb910fb95c8ca6d991
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-10-13 01:19:57 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 14648 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,36 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int m, n;
+    static int n, m;
     static int [] arr = new int[10];
     static boolean[] visited = new boolean[10];
-    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        recur(1, 0);
-        System.out.println(sb.toString());
+
+        dfs(1, 0);
     }
 
-    private static void recur(int start , int depth) {
+    public static void dfs(int cur, int depth) {
         if (depth == m) {
-            for(int i = 0; i < m; i++){
-                sb.append(arr[i]).append(' ');
+            for(int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
             }
-            sb.append("\n");
+            System.out.println();
             return;
         }
-        for(int i = start; i <= n; i++) {
+
+        // visited[i] -> 1,2,3에 대한 방문여부를 체크
+        // arr[depth] -> 깊이의 여부를 체크, 최대깊이가 곧 배열의 크기가 된다.
+        for(int i = cur; i <= n; i++) {
             if (!visited[i]) {
                 arr[depth] = i;
                 visited[i] = true;
-                recur(i + 1, depth + 1);
+                dfs(i + 1, depth + 1);
                 visited[i] = false;
             }
         }
