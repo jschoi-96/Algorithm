07494a5f8e03a3c7f1550ded6f91c807e0192c82
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-10-13 01:33:46 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 16916 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int m, n;
+    static int n, m;
     static int [] arr = new int[10];
     static boolean[] visited = new boolean[10];
     static StringBuilder sb = new StringBuilder();
@@ -15,24 +14,24 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        recur(1, 0);
-        System.out.println(sb.toString());
+        dfs( 1, 0);
+        System.out.println(sb);
     }
 
-    //같은 수를 여러 번 골라도 된다.
-    //고른 수열은 비내림차순이어야 한다.
-    private static void recur(int start, int depth) {
+    public static void dfs(int cur, int depth) {
         if (depth == m) {
-            for(int i = 0; i < m; i++){
-                sb.append(arr[i]).append(' ');
+            for(int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i = start; i <= n; i++){
+        // visited[i] -> 1,2,3에 대한 방문여부를 체크
+        // arr[depth] -> 깊이의 여부를 체크, 최대깊이가 곧 배열의 크기가 된다.
+        for(int i = cur; i <= n; i++) {
             arr[depth] = i;
-            recur(i, depth + 1);
+            dfs(i,depth + 1);
         }
     }
 }
