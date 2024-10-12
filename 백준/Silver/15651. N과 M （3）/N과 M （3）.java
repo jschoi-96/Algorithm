7846b945291242b4e23ab2599bed5ed032067ba4
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-10-13 01:32:24 +0900

    [Silver III] Title: N과 M (3), Time: 396 ms, Memory: 67820 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int n, m;
-    static int[] arr = new int[10];
+    static int [] arr = new int[10];
     static boolean[] visited = new boolean[10];
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
@@ -16,12 +14,12 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        func(0);
+        dfs( 0);
         System.out.println(sb);
     }
 
-    public static void func(int k) {
-        if (k == m) {
+    public static void dfs(int depth) {
+        if (depth == m) {
             for(int i = 0; i < m; i++) {
                 sb.append(arr[i]).append(" ");
             }
@@ -29,11 +27,11 @@ public class Main {
             return;
         }
 
+        // visited[i] -> 1,2,3에 대한 방문여부를 체크
+        // arr[depth] -> 깊이의 여부를 체크, 최대깊이가 곧 배열의 크기가 된다.
         for(int i = 1; i <= n; i++) {
-                arr[k] = i; // 이부분 ?
-                visited[i] = true;
-                func(k+1);
-                visited[i] = false;
+            arr[depth] = i;
+            dfs(depth + 1);
         }
     }
 }
