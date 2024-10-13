d16afe56a12f2bd882504f21499f2faa1d769b75
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-10-13 12:13:52 +0900

    [Silver II] Title: 촌수계산, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,50 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, a, b, m;
+    static int n, x, y, m;
     static List<List<Integer>> graph = new ArrayList<>();
     static boolean [] visited;
     static int res = -1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i <= n; i++) { // 초기화
+        for(int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
         visited = new boolean[n + 1];
 
         for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(b).add(a);
+            graph.get(a).add(b);
         }
-        dfs(a, b, 0);
+
+        dfs(x, y, 0);
         System.out.println(res);
     }
 
     public static void dfs(int start, int end, int depth) {
-        visited[start] = true;
         if (start == end) {
             res = depth;
             return;
         }
 
-        for(int next : graph.get(start)) {
-            if (!visited[next]) {
-                dfs(next, end, depth + 1);
-            }
+        for(int nxt : graph.get(start)) {
+            if (visited[nxt]) continue;
+            visited[nxt] = true;
+            dfs(nxt, end, depth + 1);
         }
     }
-
 }
