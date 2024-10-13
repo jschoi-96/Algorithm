b0b1c7debc695ba654bf3bb0be60424262fabdfb
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-10-13 11:00:45 +0900

    [level 3] Title: 여행경로, Time: 96.63 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,26 +1,27 @@
 import java.util.*;
 class Solution {
+    static boolean [] visited = new boolean[10002];
     static List<String> res = new ArrayList<>();
-    static boolean [] visited;
     public String[] solution(String[][] tickets) {
-        visited = new boolean[tickets.length];
         dfs("ICN", "ICN", 0, tickets);
-        Collections.sort(res);        
+        Collections.sort(res);
+        
         String [] answer = res.get(0).split(" ");
         return answer;
     }
     
     public void dfs(String start, String path, int depth, String[][] tickets) {
-        if (depth == tickets.length) { // 종료 조건 -> 모든 티켓을 다 쓴 경우
+        if (depth == tickets.length) {
             res.add(path);
             return;
         }
         
         for(int i = 0; i < tickets.length; i++) {
-            String next = tickets[i][1];
-            if (!visited[i] && start.equals(tickets[i][0])) {
+            String a = tickets[i][0];
+            String b = tickets[i][1];
+            if (!visited[i] && start.equals(a)) { // 방문하지 않았고, 티켓의 시작 도시가 일치하는 경우에만
                 visited[i] = true;
-                dfs(next, path + " " + next, depth + 1, tickets);
+                dfs(b, path + " " + b , depth + 1, tickets);
                 visited[i] = false;
             }
         }
