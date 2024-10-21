bd2ca889a207e97333b94bc13eaae3a1c474b5a0
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-10-21 17:22:52 +0900

    [Gold IV] Title: 연구소, Time: 816 ms, Memory: 297856 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,28 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-/*
- 1. 백트래킹과 BFS를 활용하는 문제같음
- 2. 길이가 3이 될때까지 벽을 모두 배치하고, 백트래킹을 돌면서 계속 넓이를 구함
- 3. 최댓값을 계속 갱신해줌
- */
 public class Main {
     static int n, m;
     static int [][] board;
-    static boolean [][] visited;
-    static int [] dx = {1, 0, -1, 0};
-    static int [] dy = {0, 1, 0, -1};
-    static int max_area = Integer.MIN_VALUE;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int max = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         board = new int[n][m];
-        visited = new boolean[n][m];
+        boolean [][] visited = new boolean[n][m];
+
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < m; j++) {
@@ -30,18 +26,27 @@ public class Main {
             }
         }
 
+        // 0이 존재하는 모든곳에 조합으로 벽을 다 세워본다.
+
+//        for(int i = 0; i < n; i++) {
+//            for(int j = 0; j < m; j++) {
+//                if (board[i][j] == 0) { // visited 체크 해야하나?
+//                    dfs(i,j, 1, temp, visited);
+//                    // break;
+//                }
+//            }
+//        }
         dfs(0);
-        System.out.println(max_area);
+        System.out.println(max);
     }
 
     public static void dfs(int depth) {
         if (depth == 3) {
-            // 안전거리 계산하는 메서드
             bfs();
             return;
         }
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if (board[i][j] == 0) {
                     board[i][j] = 1;
@@ -53,51 +58,48 @@ public class Main {
     }
 
     public static void bfs() {
-        int [][] tmp = new int [n][m];
-
+        int [][] temp = new int[n][m];
         for(int i = 0; i < n; i++) {
-            tmp[i] = board[i].clone();
+            for(int j = 0; j < m; j++) {
+                temp[i][j] = board[i][j];
+            }
         }
 
-        visited = new boolean[n][m]; // 매번 초기화
-        Queue<int[]> queue = new LinkedList<>();
-        for(int i = 0; i < n; i++){
+        Queue<int []> q = new LinkedList<>();
+        for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if (tmp[i][j] == 2) {
-                    queue.add(new int[]{i, j}); // 바이러스 큐에 집어넣음
-                    visited[i][j] = true;
-                    while(!queue.isEmpty()) {
-                        int [] cur = queue.poll();
-                        int x = cur[0];
-                        int y = cur[1];
-
-                        for(int dir = 0; dir < 4; dir++) {
-                            int nx = x + dx[dir];
-                            int ny = y + dy[dir];
-                            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                            if (!visited[nx][ny] && tmp[nx][ny] == 0) {
-                                queue.add(new int[]{nx, ny});
-                                tmp[nx][ny] = 2;
-                                visited[nx][ny] = true;
-                            }
-                        }
-                    }
+                if (temp[i][j] == 2) {
+                    q.add(new int[]{i, j});
                 }
             }
         }
 
-        calculate(tmp);
+        while(!q.isEmpty()) {
+            int [] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            for(int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if (temp[nx][ny] == 0) {
+                    temp[nx][ny] = 2;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+
+        int cnt = count(temp);
+        max = Math.max(cnt, max);
     }
 
-    public static void calculate(int [][] tmp) {
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if (tmp[i][j] == 0) {
-                    count++;
-                }
+    public static int count(int [][] temp) {
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if (temp[i][j] == 0) cnt++;
             }
         }
-        max_area = Math.max(max_area, count);
+        return cnt;
     }
 }
