6ec9f5887ed5c9ac92084760683911d7e5dd0fcb
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-10-24 15:35:38 +0900

    [Gold V] Title: 치킨 배달, Time: 188 ms, Memory: 15612 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,17 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
     static int [][] board;
-    static int [] dx = {1,0,-1,0};
-    static int [] dy = {0, 1, 0, -1};
-    static List<int[]> home = new ArrayList<>();
-    static List<int[]> chicken = new ArrayList<>();
-    static int[] res;
-    static int answer = Integer.MAX_VALUE;
+    static int [] arr;
+    static List<int[]> chickens = new ArrayList<>();
+    static List<int[]> houses = new ArrayList<>();
+    static int min_total = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,44 +19,50 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         board = new int[n][n];
-        res = new int[m];
-        for (int i = 0; i < n; i++) {
+        arr = new int[m];
+
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            for(int j = 0; j < n; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
-                if (board[i][j] == 1) home.add(new int[]{i,j});
-                else if (board[i][j] == 2) chicken.add(new int[]{i,j});
+                if (board[i][j] == 1) houses.add(new int[]{i,j});
+                else if (board[i][j] == 2) chickens.add(new int[]{i,j});
             }
         }
 
         dfs(0, 0);
-        System.out.println(answer);
+        System.out.println(min_total);
     }
 
-    private static void dfs(int start, int depth) {
+    public static void dfs(int start, int depth) {
         if (depth == m) {
-            calculate();
+            cal();
             return;
         }
 
-        for(int i = start; i < chicken.size(); i++) {
-            res[depth] = i;
+        for(int i = start; i < chickens.size(); i++) {
+            arr[depth] = i;
             dfs(i + 1, depth + 1);
         }
     }
 
-    private static void calculate() {
-        int total = 0;
-        for (int[] h : home) {
+    public static void cal() {
 
+        int total = 0;
+        for(int [] house : houses) {
+            int x = house[0];
+            int y = house[1];
             int min_dist = Integer.MAX_VALUE;
-            for (int idx : res) {
-                int [] c = chicken.get(idx);
-                int dist = Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
+            for(int idx : arr) {
+                int [] chicken = chickens.get(idx);
+                int nx = chicken[0];
+                int ny = chicken[1];
+                int dist = Math.abs(nx - x) + Math.abs(ny - y);
                 min_dist = Math.min(min_dist, dist);
             }
             total += min_dist;
         }
-        answer = Math.min(answer, total);
+        min_total = Math.min(min_total , total);
+        //System.out.println(total);
     }
 }
