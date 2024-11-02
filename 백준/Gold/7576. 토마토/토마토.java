9c3f8bcca61fecd4c72d244de0895e09593e363b
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-11-02 16:46:58 +0900

    [Gold V] Title: 토마토, Time: 592 ms, Memory: 129980 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,68 +1,67 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int m, n;
-    static int[][] board = new int[1002][1002];
-    static int[][] dist = new int[1002][1002];
-    static Queue<int[]> queue = new LinkedList<>();
-    static int[] dx = { 1, 0, -1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
-
-    public static void main(String[] args) throws Exception {
-
+    static int n, m;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean [][] visited;
+    static int [][] board;
+    static int min_days = 0;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < m; i++) {
+        board = new int[n][m];
+        visited = new boolean[n][m];
+
+        Queue<int []> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
-                if (board[i][j] == 1) { // 토마토가 익은 경우 큐에 넣는다
-                    queue.add(new int[] { i, j });
-                }
-                if (board[i][j] == 0) { // 익지 않은 경우는 -1로 초기화
-                    dist[i][j] = -1;
+                if (board[i][j] == 1) {
+                    q.add(new int[]{i, j, 0});
                 }
             }
         }
 
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int x = cur[0];
-            int y = cur[1];
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = x + dx[dir];
-                int ny = y + dy[dir];
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n)
-                    continue;
-                if (dist[nx][ny] >= 0)
-                    continue; // 이미 방문했다면, 지나친다.
-                dist[nx][ny] = dist[x][y] + 1;
-                queue.add(new int[] { nx, ny });
-            }
-        }
-
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[i][j] == -1) {
-                    ans = -1;
+        bfs(q);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 0 && !visited[i][j]) {
                     System.out.println(-1);
                     return;
                 }
-                ans = Math.max(ans, dist[i][j]);
             }
-
         }
-        System.out.println(ans);
+        System.out.println(min_days);
+    }
 
+    public static void bfs(Queue<int[]> q) {
+        while(!q.isEmpty()) {
+            int [] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int day = cur[2];
+            for(int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if (visited[nx][ny] || board[nx][ny] != 0) continue;
+                visited[nx][ny] = true;
+                //System.out.println(day);
+                min_days = Math.max(min_days, day + 1);
+                q.add(new int[]{nx, ny, day + 1});
+            }
+        }
     }
 }
