9821305d05e89e127758dca271b6effa56b52b91
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-12-23 20:08:37 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,12 +1,9 @@
 -- 코드를 입력하세요
--- 1. 중성화 되어있으면 'O', 안되어있으면 'X'로 표시
--- 2. 아이디, 이름, 중성화 여부 출력
--- 3. 아이디 순으로 조회
-SELECT ANIMAL_ID, NAME,
+SELECT ANIMAL_ID, NAME, 
     CASE 
-        WHEN SEX_UPON_INTAKE LIKE '%Neutered%' THEN 'O'
-        WHEN SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+        WHEN SEX_UPON_INTAKE like 'Neutered%' THEN 'O'
+        WHEN SEX_UPON_INTAKE like 'Spayed%' THEN 'O'
         ELSE 'X'
-    END as 중성화
+    END '중성화'
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID
