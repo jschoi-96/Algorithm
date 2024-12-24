4f3d4876f6fb3d6f132ed1cb165e8ed498ab3b6a
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-12-24 10:13:24 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,8 +1,7 @@
 -- 코드를 입력하세요
--- 1. 보호 시작일보다 입양일이 빠른 (즉, 양쪽의 데이터 값을 알아야 하나?)
-SELECT O.ANIMAL_ID, O.NAME
-FROM ANIMAL_OUTS O
-INNER JOIN ANIMAL_INS I
-ON O.ANIMAL_ID = I.ANIMAL_ID
-WHERE O.DATETIME < I.DATETIME
-ORDER BY I.DATETIME
+SELECT i.animal_id, i.name
+FROM animal_ins i
+join animal_outs o
+on i.animal_id = o.animal_id
+where i.datetime > o.datetime
+order by i.datetime
