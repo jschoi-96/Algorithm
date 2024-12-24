43bb6135d0ef8900331c3408f6a844d38d686c34
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-12-24 10:35:59 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT i.animal_id, i.name
+from animal_ins i
+left join animal_outs o
+on i.animal_id = o.animal_id
+where o.animal_id is not null
+order by datediff(i.datetime, o.datetime)
+limit 2
