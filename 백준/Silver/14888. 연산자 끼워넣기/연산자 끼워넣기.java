c5a9e4ebaad707f80a1655e32e49091cbe33203f
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-01-06 18:50:54 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 116 ms, Memory: 15448 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,23 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
     static int n;
-    static int [] nums;
-    static int [] cal = new int [4];
+    static int [] cal = new int[4];
+    static int [] arr;
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        nums = new int [n];
 
+        arr = new int[n];
+        // +, -, x, /
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
@@ -25,27 +25,36 @@ public class Main {
             cal[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 1. 계산 배열을 순회하며
-        dfs(nums[0], 1);
+        dfs(1, arr[0]); // 초기값을 저장
         System.out.println(max);
         System.out.println(min);
     }
 
-    private static void dfs(int num, int depth) {
+    public static void dfs(int depth, int cur) {
         if (depth == n) {
-            max = Math.max(max, num);
-            min = Math.min(min, num);
+            max = Math.max(cur, max);
+            min = Math.min(cur, min);
             return;
         }
 
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 4; i++) {
             if (cal[i] > 0) {
                 cal[i]--;
-                switch(i) {
-                    case 0: dfs(num + nums[depth] , depth + 1); break;
-                    case 1: dfs(num - nums[depth] , depth + 1); break;
-                    case 2: dfs(num * nums[depth] , depth + 1); break;
-                    case 3: dfs(num / nums[depth] , depth + 1); break;
+                if (i == 0) {
+                    dfs(depth + 1, cur + arr[depth]);
+                    //dfs(depth + 1, cur - arr[depth]);
+                }
+                else if (i == 1) {
+                    dfs(depth + 1, cur - arr[depth]);
+                    //dfs(depth + 1, cur + arr[depth]);
+                }
+                else if (i == 2) {
+                    dfs(depth + 1, cur * arr[depth]);
+                    //dfs(depth + 1, cur / arr[depth]);
+                }
+                else if (i == 3) {
+                    dfs(depth + 1, cur / arr[depth]);
+                    //dfs(depth + 1, cur * arr[depth]);
                 }
                 cal[i]++;
             }
