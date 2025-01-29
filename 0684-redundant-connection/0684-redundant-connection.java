e5624f72088e87bd202561ca308a4e6ea5976bf5
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-01-29 14:25:31 +0900

    Time: 83 ms (5.03%), Space: 45.6 MB (5.2%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,10 +1,10 @@
 class Solution {
-    // 1. 간선을 하나 끊어도 정점이 전부 연결될 수 있는 곳을 찾음
-    // 2. 간선이 여러개라면 가장 나중에 등장한 것을 삭제
-    private List<List<Integer>> graph = new ArrayList<>();
-    private boolean [] visited;
+    List<List<Integer>> graph = new ArrayList<>();
+    int n;
     public int[] findRedundantConnection(int[][] edges) {
-        int n = edges.length;
+        n = edges.length;
+        int [] res = new int[2];
+
         for(int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
@@ -16,37 +16,38 @@ class Solution {
             graph.get(v).add(u);
         }
 
-        int [] res = new int[2];
         for(int [] edge : edges) {
             int u = edge[0];
             int v = edge[1];
-            graph.get(u).remove((Integer)v);
-            graph.get(v).remove((Integer)u);
+            graph.get(u).remove((Integer) v);
+            graph.get(v).remove((Integer) u);
 
-            visited = new boolean[n+1];
-            dfs(1, n);
-            if (allVisited(n)) {
-                res[0] = u; 
+            boolean [] visited = new boolean[n+1];
+            dfs(u, visited);
+            if (checkVisited(n, visited)) {
+                res[0] = u;
                 res[1] = v;
+                // System.out.println(u + " " + v);
             }
+
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
         return res;
     }
 
-    public void dfs(int start, int n) {
+    public void dfs(int start, boolean[] visited) {
         visited[start] = true;
-        for(int nxt : graph.get(start)) {
-            // System.out.println("start: " + start + " nxt: " + nxt);
-            if (!visited[nxt]) {
-                visited[nxt] = true;
-                dfs(nxt, n);
+
+        for(int next : graph.get(start)) {
+            if (!visited[next]) {
+                visited[next] = true;
+                dfs(next, visited);
             }
         }
     }
 
-    public boolean allVisited(int n) {
+    public boolean checkVisited(int n, boolean[] visited) {
         for(int i = 1; i <= n; i++) {
             if (!visited[i]) return false;
         }
