fc0960210c7aff5bdca82586841d42a9d96a63ea
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-02-27 17:20:56 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.94 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -2,32 +2,29 @@ import java.util.*;
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
+        
+        PriorityQueue<int []> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0] , b[0]));
+        
         int currentServer = 0;
-        
-        // [종료 시간, 현재 서버 수]
-        PriorityQueue<int []> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0], b[0]));
-        
-        for(int i = 0; i < 24; i++) {
-            int people = players[i];
+        for(int i = 0; i < players.length; i++) {
+            if (players[i] < m) continue; // 사용자 m명 미만인 경우
+            int needServer = players[i] / m;
             
-            if (people < m) continue;
-            
-            while(!pq.isEmpty() && pq.peek()[0] <= i) {
+            if (!pq.isEmpty() && pq.peek()[0] <= i) { // 
                 int [] cur = pq.poll();
                 currentServer -= cur[1];
             }
             
-            int needServer = players[i] / m;
-            int addServer = Math.max(0, needServer - currentServer); // 부족한 서버 수 
-            //System.out.println("cur: " + currentServer +  " need: " + needServer + " addServer: " + addServer);
-            if (addServer > 0) { // 부족한 서버만큼 서버 추가해야함.
-                pq.add(new int []{i+k , addServer});
-                currentServer += addServer; 
+            int addServer = needServer - currentServer;
+            if (currentServer < needServer) { // 서버 충원해야함.
+                pq.add(new int[]{i + k, addServer});
+                //System.out.println(needServer - currentServer);
+                currentServer += addServer;
                 answer += addServer;
-            }       
+            }
+
             
         }
-        
         return answer;
     }
 }
