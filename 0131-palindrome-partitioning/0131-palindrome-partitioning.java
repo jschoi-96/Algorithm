8b7a7007bf0d9de88ca81702a28aab6e9d9bd292
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-03-06 14:49:10 +0900

    Time: 216 ms (5.03%), Space: 56.5 MB (81.74%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,40 +1,33 @@
 class Solution {
     List<List<String>> res = new ArrayList<>();
-    // 1. 문자열을 1, 2, 3... 씩 증가시켜가며 slice를 해준다.
-    // 2. len이 문자열의 끝에 도달했을 때 리스트에 존재하는 문자열들이 팰린드롬인지 판단
-    // 3. 팰린드롬이라면, res에 넣어주고 아니라면 continue. 
     public List<List<String>> partition(String s) {
-        if (s.length() == 0) {
-            return res;
-        }
-
-        dfs(s, new ArrayList<>(), 0);
+        dfs(s, 0, new ArrayList<>());
         return res;
     }
 
-    public void dfs (String s, List<String> tmp, int idx) {
-
+    public void dfs(String s, int idx, List<String> tmp) {
         if (idx == s.length()) {
             res.add(new ArrayList<>(tmp));
             return;
         }
 
-        for(int i = idx + 1; i <= s.length(); i++) {
-            String sub = s.substring(idx, i);
-            if (isPalindrome(sub)) {
-                tmp.add(sub);
-                // System.out.println("idx: " + idx + " " + "i: " + i + " " + sub);
-                dfs(s, tmp, i);
+        for(int i = idx; i < s.length(); i++) {
+            String str = s.substring(idx, i + 1);
+            if (isValid(str)) {
+                tmp.add(str);
+                System.out.println(str);
+                dfs(s, i + 1, tmp);
                 tmp.remove(tmp.size() - 1);
             }
         }
     }
 
-    public boolean isPalindrome(String s) {
-        int lo = 0, hi = s.length() - 1;
+    public boolean isValid(String str) {
+        int lo = 0, hi = str.length() - 1;
         while(lo < hi) {
-            if (s.charAt(lo) != s.charAt(hi)) return false;
-            lo++; hi--;
+            if (str.charAt(lo) != str.charAt(hi)) return false;
+            lo++;
+            hi--;
         }
         return true;
     }
