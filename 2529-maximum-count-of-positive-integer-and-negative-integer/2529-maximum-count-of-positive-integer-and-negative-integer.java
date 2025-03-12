5c60ca69c613ace984cdf08c221efb0e51dad504
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-03-12 15:26:50 +0900

    Time: 9 ms (1.67%), Space: 45.6 MB (8.64%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,22 +1,18 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        Arrays.sort(nums);
+        int min_cnt = binarySearch(nums, 0);
+        int max_cnt = nums.length - binarySearch(nums, 1);
+        System.out.println(min_cnt + " " + max_cnt);
+        return Math.max(min_cnt, max_cnt);
+    }
 
-        if (nums[0] > 0 || nums[nums.length - 1] < 0) return nums.length;
-
-        int lo = 0, hi = nums.length - 1;
-
-        int min = 0, max = 0;
-        while(nums[lo] < 0) {
-            lo++;
+    public int binarySearch(int[] nums, int target) {
+        int lo = 0, hi = nums.length;
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] >= target) hi = mid;
+            else lo = mid + 1;
         }
-
-        while(nums[hi] > 0) {
-            hi--;
-            max++;
-        }
-
-        //System.out.println(lo + " " + max);
-        return Math.max(lo, max);
+        return lo;
     }
 }
