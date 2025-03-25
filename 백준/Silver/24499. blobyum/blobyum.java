e67a01f1d8426c01073de6969db26306c7eeced2
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-03-25 18:28:13 +0900

    [Silver IV] Title: blobyum, Time: 256 ms, Memory: 23980 KB -BaekjoonHub

--- 백준/Silver/24499. blobyum/blobyum.java
+++ 백준/Silver/24499. blobyum/blobyum.java
@@ -5,29 +5,28 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int n, k;
-    static long [] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        arr = new long[n+1];
+        int [] arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        long window = 0;
-        long max = 0;
-        for(int i = 0; i < n + k; i++) {
-            window += arr[i % n];
-            if (i >= k - 1) {
-                max = Math.max(max, window);
-                window -= arr[(i - (k - 1)) % n];
-            }
+        int preSum = 0;
+        for(int i = 0; i < k; i++) {
+            preSum += arr[i];
+        }
 
+        int max = preSum;
+        for(int i = k; i < n + k; i++) {
+            preSum -= arr[i-k];
+            preSum += arr[i % n];
+            max = Math.max(max, preSum);
         }
         System.out.println(max);
     }
