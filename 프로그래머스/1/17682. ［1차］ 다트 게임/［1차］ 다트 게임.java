20aaac688434c7a4fc17a3e50b66503965d83470
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-04-06 17:49:36 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,49 +1,68 @@
-import java.util.*;
 class Solution {
+    /*
+        S,D,T는 각각 1,2,3제곱 / 
+        * -> 해당 점수와 바로 이전의 점수를 2배
+        # -> 해당 점수를 마이너스 처리
+        
+        *는 첫번째에도 나올 수 있기 때문에 인덱스 주의할것
+        1. String을 순회하며 해당 숫자의 인덱스를 파악 (이 때, 10엣지 처리)
+        1-1. 숫자들만의 인덱스를 따로 체크
+        2. 문자열이 있을 때, 앞에 있던 숫자들에 대한 계산을 해줘야함.
+        3. S,D,T,#에 대해서 계산을 해주는 메서드를 생성. 
+        4. *에 대해서 계산하는 메서드를 생성(*은 이전 인덱스도 알아야하기 때문에 따로 생성)
+    */
     public int solution(String dartResult) {
         int answer = 0;
         
-        int [] arr = new int[3];
         int idx = 0;
-        for(int i = 0; i < dartResult.length(); i++){
-            char c = dartResult.charAt(i);
-            if (c == 'S') {
-                int num = convert(i, dartResult);
-                arr[idx++] = num;
-            }
-            else if (c == 'D') {
-               int num = (int)Math.pow(convert(i, dartResult), 2);
-                arr[idx++] = num;
+        int numIdx = 0;
+        int prev = 0; // 이전 값을 저장
+        
+        int [] arr = new int[dartResult.length()];
+        // 임의로 길이 설정
+        
+        for(char c : dartResult.toCharArray()) {
+            int check = c - '0';
+            if (check >= 0 && check <= 9) {
+                // 10 엣지 처리
+                if (check == 0 && (idx > 0 && dartResult.charAt(idx - 1) == '1')) prev = 10;
+                else prev = check;
+                numIdx++; // 숫자 인덱스 +1
+            }   
+            
+            else { // 여기서
+                if (c == '*') {
+                    if (numIdx > 0) {
+                        arr[numIdx - 1] *= 2;
+                    }
+                    arr[numIdx] *= 2;
+                }
+                
+                else {
+                    int cur = notStar(prev, c);
+                    prev = cur;
+                    arr[numIdx] = prev;
+                }
             }
             
-            else if (c == 'T') {
-                int num = (int)Math.pow(convert(i, dartResult), 3);
-                arr[idx++] = num;
-            }
-           
-            else if (c == '*') {
-                arr[idx-1] *= 2;
-                if (idx - 2 < 0) continue;
-                arr[idx-2] *= 2;
-            }
-            
-            else if (c == '#') {
-                if (idx - 1 < 0) continue;
-                arr[idx-1] *= -1;
-            }
+            // for(int i = 0; i < arr.length; i++) {
+            //     if (arr[i] != 0) System.out.print(arr[i] + " ");
+            // }
+            // System.out.println();
+            idx++;
         }
         
-       for(int i = 0; i < arr.length; i++) {
-            answer += arr[i];
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) answer += arr[i];
         }
         return answer;
     }
     
-    public int convert(int i, String dartResult) {
-        int num = Integer.parseInt(dartResult.substring(i-1,i));
-        if (i - 2 >= 0 && num == 0 && dartResult.substring(i-2,i-1).equals("1")) {
-            return 10;
-        }
-        return num;
+    public int notStar(int prev, char c) {
+        if (c == 'S') return prev;
+        else if (c == 'D') return prev*prev;
+        else if (c == 'T') return prev*prev*prev;
+        else if (c == '#') return prev*-1;
+        return 0;
     }
 }
