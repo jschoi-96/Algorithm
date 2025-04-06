1723b866f9830cce4f79e29beb614ed846331294
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-04-06 19:33:10 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,34 +1,36 @@
+import java.util.*;
 class Solution {
+    /*
+        0 -> 알아볼수 없는 문제
+        해당 값을 바꾸면서 최저, 최고순위를 찾음
+        1. HashSet에 win_nums를 저장
+        2. lottos를 순회하며 set이 해당 번호를 contains하는지 추적
+        3. 이 때, 0의 갯수도 같이 추적함.
+        4. 일치하는 번호의 개수는 최소: contains개수, 최대: contains개수 + 0의 개수
+        5. 순위 계산은 6 - 일치 갯수 + 1
+    */
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
+        int n = lottos.length; // 로또와 win 모두 길이 동일
         
-        int win_count = 0;
-        int zero = 0;
-      
-        for(Integer lotto : lottos) {
-            if (lotto == 0) {
-                zero++;
-            }
-            for(Integer win : win_nums) {
-               
-                if (win == lotto) {
-                    win_count++;
-                    break;
-                }
-                
-            }
+        HashSet<Integer> set = new HashSet<>();
+        for(int win : win_nums) set.add(win);
+        
+        int has = 0; // 일치 개수
+        int zero = 0; // 0의 개수
+        for(int lotto : lottos) {
+            if (set.contains(lotto)) has++;
+            else if (lotto == 0) zero++;
         }
-        int [] result = new int[2];
-        result[0] = win_count + zero;
-        result[1] = win_count;
-        for(int i = 0; i < result.length; i++) {
-            if (result[i] == 6) answer[i] = 1;
-            else if (result[i] == 5) answer[i] = 2;
-            else if (result[i] == 4) answer[i] = 3;
-            else if (result[i] == 3) answer[i] = 4;
-            else if (result[i] == 2) answer[i] = 5;
-            else answer[i] = 6;
-        }
+        
+        int lo = has;
+        int hi = has + zero;
+        
+        // 맞춘 갯수가 0개라면 1로 엣지처리 
+        if (lo == 0) lo = 1;
+        if (hi == 0) hi = 1;
+        answer[1] = 6 - lo + 1;
+        answer[0] = 6 - hi + 1;
         return answer;
     }
 }
