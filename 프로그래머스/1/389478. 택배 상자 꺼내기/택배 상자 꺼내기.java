a5e78ef13d3a88ddaa6ba8a1b7259dc75ae37381
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-04-08 02:45:57 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.05 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,51 +1,62 @@
 class Solution {
+    int idx = 1;
+    int nums = 0;
+    int target = 0;
+    
+    int [] pos = new int[2];
     public int solution(int n, int w, int num) {
         int answer = 0;
+        nums = n;
+        target = num;
         
-        int height = n % w == 0 ? n / w : (n / w) + 1;
-        int [][] board = new int[height][w];
+        // 1.높이 정의
+        int h = 0;
+        if (n % w != 0) h = n/w + 1;
+        else h = n/w; 
         
-        for(int i = 0; i < height; i++) {
-            for(int j = 0; j < w; j++) board[i][j] = 0;
+        int[][] board = new int[h][w];
+                        
+        int cur = h-1; // 시작 높이 
+        
+        while (cur >= 0) {
+            right(board, cur,w);
+            cur -= 1;
+            if (cur < 0) break;
+            left(board, cur,w);
+            cur -= 1;
         }
         
-        boolean flag = true;
-        int val = 1;
+        int x = pos[0]; // 뽑으려는 상자의 높이 좌표
+        int y = pos[1];
         
-        int [] pos = new int[2];
-        for(int i = height - 1; i >= 0; i--) {
-            if (flag) {
-                for(int j = 0; j < w; j++) {
-                    if (val > n) break;
-                    board[i][j] = val;
-                    if (val == num) {
-                        pos[0] = i;
-                        pos[1] = j;
-                    }
-                    val++;
-                }
-                flag = false;
+        int cnt = 0;
+        for(int i = 0; i <= x; i++) {
+            if (board[i][y] == 0) continue;
+            cnt++;
+        }
+        
+        return cnt;
+    }
+    
+    public void right(int[][] board, int cur, int w) {
+        for(int i = 0; i < w; i++) {
+            if (idx > nums) continue;
+            board[cur][i] = idx++;
+            if (board[cur][i] == target) {
+                pos[0] = cur;
+                pos[1] = i;
             }
-            
-            else {
-                for(int j = w - 1; j >= 0; j--) {
-                    if (val > n) break;
-                    board[i][j] = val;
-                    if (val == num) {
-                        pos[0] = i;
-                        pos[1] = j;
-                    }
-                    val++;
-                }
-                flag = true;
+        }
+    }
+    
+    public void left(int[][] board, int cur, int w) {
+        for(int i = w - 1; i >= 0; i--) {
+            if (idx > nums) continue;
+            board[cur][i] = idx++;
+            if (board[cur][i] == target) {
+                pos[0] = cur;
+                pos[1] = i;
             }
         }
-        
-        for(int i = 0; i <= pos[0]; i++) {
-            //System.out.println(board[i][pos[1]]);
-            if (board[i][pos[1]] == 0) continue;
-            answer++;
-        }
-        return answer;
     }
 }
