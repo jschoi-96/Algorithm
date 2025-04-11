4c6ac08313b800a7e4a965a3b1041365393cf9fc
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-04-12 01:33:05 +0900

    [level 2] Title: 주차 요금 계산, Time: 6.71 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,77 +1,76 @@
 import java.util.*;
 class Solution {
-    // 기본시간 미만일 떄 -> 기본요금
-    // 넘을 때 -> 기본요금 + (누적 - 기본시간) / 단위 시간 * 단위 요금
-    public int[] solution(int[] fees, String[] records) {
+    public int[] solution(int[] fees, String[] records) {        
+        Map<Integer, Integer> inMap = new HashMap<>(); // 차번호, 입차시간 저장하는 맵
         
-        Map<String, Integer> map = new HashMap<>(); // [차번호, 마지막 입차시간]
-        Map<String, Integer> timeMap = new HashMap<>(); // [차번호, 누적시간]
-        
+        Map<Integer, Integer> sumMap = new HashMap<>(); // 차번호, 누적시간 저장하는 맵.
         for(String record : records) {
-            String [] arr = record.split(" ");
-            String time = arr[0];
-            String carNum = arr[1];
-            String command = arr[2];
+            String [] str = record.split(" ");
+            int time = cal(str[0]);
+            int car = Integer.parseInt(str[1]);
+            String command = str[2];
             
-            String [] timeSplit = arr[0].split(":");
-            String hour = timeSplit[0];
-            String min = timeSplit[1];
-            
-            int totalTime = Integer.parseInt(hour) * 60 + Integer.parseInt(min);
-            //System.out.println(totalTime);
-            if (command.equals("IN")) {
-                map.put(carNum, totalTime); // 현재 입차시간 저장
+            if (command.equals("IN")) { // 입차인 경우에 맵에 값을 넣어줌.
+                inMap.put(car, time);
             }
             
-            else {
-                int inTime = map.get(carNum); // 기존 입차시간
-                int sum = totalTime - inTime;
-                map.put(carNum, -1); // 입차시간을 초기화
-
-                timeMap.put(carNum, timeMap.getOrDefault(carNum, 0) + sum); // 누적시간을 저장 
+            else if (command.equals("OUT")) {
+                int prev = inMap.get(car);
+                int val = time - prev; // 출차 시간에서 입차시간을 빼서 누적 시간에 넣어준다.
+                sumMap.put(car, sumMap.getOrDefault(car, 0) + val);
+                
+                inMap.put(car, -1); // 이 때, 입차시간은 초기화 시켜줘야한다!!
+            }
+            // System.out.println(time + " " + car + " " + command);
+        }
+        
+        // 입차시간을 저장한 맵을 순회하며 0으로 초기화가 되지 않은 값을 찾는다.
+        // 이 경우에는, 23:59분에서 마지막 입차시간을 빼서 다시 누적시간 맵에 갱신해준다.
+        
+        for(Integer key : inMap.keySet()) {
+            int val = inMap.get(key);
+            if (val != -1) {
+                int last = cal("23:59");
+                last -= val;
+                sumMap.put(key, sumMap.getOrDefault(key, 0) + last);
             }
         }
         
-        int lastTime = 23 * 60 + 59;
-        for(String key : map.keySet()) {
-            if (map.get(key) != -1) { // 여기서 0이 아니란 뜻은 출차 기록이 없다는 뜻. 따라서 마지막 시간에서 빼서 더해줌
-                timeMap.put(key, timeMap.getOrDefault(key, 0) + lastTime - map.get(key));
-            }
-        }
+        // 누적시간을 저장한 맵에서 차량번호를 오름차순으로 정렬한다.
+        List<Integer> list = new ArrayList<>(sumMap.keySet());
+        Collections.sort(list);
         
-        int default_time = fees[0];
-        int default_price = fees[1];
-        int unit_time = fees[2];
-        int unit_price = fees[3];
         
-        int[] answer = new int[map.size()];
+        int[] answer = new int[list.size()];
         int idx = 0;
-        
-        List<String> cars = new ArrayList<>(timeMap.keySet());
-        Collections.sort(cars);
-        
-        for (String time : cars) {
-            int sum = timeMap.get(time);
-            if (sum <= default_time) {
-                // System.out.println(time + " " + default_price);
-                answer[idx++] = default_price;
-                // System.out.println("idx: " + idx);
-            }
+
+        for(Integer car : list) {
+            int t = sumMap.get(car);
+            // System.out.println(car + " " + t + " " + calFee(fees, t));    
             
-            else {
-                int newTime = (sum - default_time) / unit_time;
-                if ((sum - default_time) % unit_time != 0) newTime += 1;
-                int newSum = default_price + newTime * unit_price;
-                // System.out.println(time + " " + newSum);
-                answer[idx++] = newSum;
-                // System.out.println("idx: " + idx);
-            }
-            
-            
+            int result = calFee(fees, t);
+            answer[idx++] = result;
+
         }
         
-
- 
         return answer;
+    }
+    
+    public int cal(String time) {
+        String [] t = time.split(":");
+        // 60분 = 1시간
+        return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+    }
+    
+    public int calFee(int[] fees, int t) {
+        // 기본 시간 이하일 때 기본요금 청구
+        if (t <= fees[0]) return fees[1];
+        else {
+            int time = t - fees[0];
+            int diff = time / fees[2];
+            if (time % fees[2] != 0) diff += 1; // 나눠떨어지지 않는 경우에 1더해서 올려줌(?)
+            
+            return fees[1] + diff * fees[3];
+        }
     }
 }
