10442a424e11055e6b865cd234290f4492b5d57c
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-04-18 16:39:20 +0900

    [Silver II] Title: 랜선 자르기, Time: 164 ms, Memory: 17548 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -5,42 +5,40 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int k, n;
-    static int [] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        k = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
 
-        arr = new int[k];
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < k; i++) {
+        int [] arr = new int[k];
+        for (int i = 0; i < k; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr); // nlog(n)
-        // 4 + 3 + 2 + 2
+        // 1. arr를 오름차순으로 정렬
+        Arrays.sort(arr);
 
-        long lo = 0; // 랜선의 길이 최댓값이 2^31 -1
-        long hi = arr[k-1];
+        // 2. 이분탐색을 통해 mid값으로 계속 나눴을 때 k값과 비교를 진행한다.
+        long hi = arr[k - 1];
         hi++;
 
+        long lo = 0;
         while(lo < hi) {
             long mid = (lo + hi) / 2;
-            long result = 0;
-            for(int i = 0; i < arr.length; i++) {
-                result += (arr[i] / mid);
-            }
+            if (mid == 0) continue;
+            long cnt = 0;
+            for(int num : arr) cnt += num / mid;
 
-            if (result < n) { // n보다 작다는 뜻은 상한선을 낮춰서 자르는 갯수를 더 많이 생성해야 함
-                hi = mid;
-            }
+            // 최대 랜선의 길이를 구해야 하므로 cnt가 k이상일 때 하방값을 올려준더.
+            if (cnt >= n) lo = mid + 1;
+            else hi = mid;
 
-            else {
-                lo = mid + 1;
-            }
+            // System.out.println(lo + " " + hi);
         }
+
         System.out.println(lo - 1);
     }
 }
