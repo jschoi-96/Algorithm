b2ed18c083469c4a46cdd4fa32b3adb79b5dab1c
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-04-29 17:58:29 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 33.85 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,62 +1,70 @@
 import java.util.*;
 class Solution {
-    static Map<String,Integer> map = new HashMap<>();
-    static List<String> answerList = new ArrayList<>();
-    public String[] solution(String[] orders, int[] course) {   
-        
-        // 1. 각 order 정렬
-        for(int i = 0; i < orders.length; i++) {
-            char [] chars = orders[i].toCharArray();
-            Arrays.sort(chars);
-            orders[i] = String.valueOf(chars);
-        }
-        
-        // 2. 각 order를 기준으로 courseLength 만큼 조합 만들기
-        for(int courseLength : course) {
-            for (String order : orders) {
-                dfs("", order, courseLength);
-            }
-            
-            // 3. 가장 많은 조합을 answer에 저장
-            
-            if (!map.isEmpty()) {
-                // 4. 해쉬맵의 value를 리스트에 담고 max값을 찾는다.
-                List<Integer> countList = new ArrayList<>(map.values());
-                int max = Collections.max(countList);
+    static int max = 0;
+    public String[] solution(String[] orders, int[] course) {
                 
-                if (max > 1) {
-                    for(String key : map.keySet()) {
-                        if (map.get(key) == max) {
-                            answerList.add(key);
-                        }
-                    }
+        Map<Integer, List<Character>> map = new HashMap<>();
+        for(int i = 0; i < orders.length; i++) {
+            map.put(i+1, new ArrayList<>());
+        }
+        
+        int idx = 1;
+        for(String order : orders) {
+            
+            for(int i = 0; i < order.length(); i++) {
+                char c = order.charAt(i);
+                map.get(idx).add(c);
+            }
+            
+            Collections.sort(map.get(idx)); // 여기서 오름차순으로 정렬
+            idx++;
+        }
+        
+        for(Integer key : map.keySet()) {
+            //System.out.println(key + " " + map.get(key));
+        }
+        
+        // 길이 순회하며 백트래킹을 돌리고, 가장 많이 등장한 코스를 찾는다. 
+        
+        List<String> res = new ArrayList<>();
+        for(int len : course) {
+            Map<String, Integer> tmp = new HashMap<>();
+            max = 0; // 최대 등장횟수
+            for(Integer key : map.keySet()) {
+                dfs(len, map.get(key), tmp, 0, 0, new ArrayList<>());
+            }
+            
+            List<String> keys = new ArrayList<>(tmp.keySet());
+            for(String key : keys) {
+                if (max >= 2 && tmp.get(key) == max) {
+                    res.add(key);
                 }
-                map.clear();
             }
         }
         
-        Collections.sort(answerList);
-        String [] answer = new String[answerList.size()];
-        
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = answerList.get(i);
-        }
+        Collections.sort(res);
+        String[] answer = new String[res.size()];
+        for(int i = 0; i < res.size(); i++) answer[i] = res.get(i);
         return answer;
     }
     
-    // order - 현재까지 조합된 course
-    // others - 사용되지 않은 알파벳
-    // count - 몇 개를 조합해야 하는지
-    private void dfs(String order, String others, int count) {
-        
-        if (count == 0) {
-            map.put(order, map.getOrDefault(order, 0) + 1);
+    public void dfs(int len, List<Character> list, Map<String, Integer> tmp, int curLen, int idx, List<Character> cur) {
+        if (curLen == len) {
+            String key = "";
+            for(char c : cur) {
+                key += c;
+            }
+            tmp.put(key, tmp.getOrDefault(key, 0) + 1);
+            max = Math.max(max, tmp.get(key));
+            //System.out.println(key);
             return;
         }
         
-        for(int i = 0; i < others.length(); i++) {
-            // i번째 까지는 조합을 했으므로, i+1부터는 사용되지 않은 알파벳
-            dfs(order + others.charAt(i), others.substring(i+1), count - 1);
+        //System.out.println("?");
+        for(int i = idx; i < list.size(); i++) {
+            cur.add(list.get(i));
+            dfs(len, list, tmp, curLen + 1, i + 1, cur);
+            cur.remove(cur.size() - 1);
         }
     }
 }
