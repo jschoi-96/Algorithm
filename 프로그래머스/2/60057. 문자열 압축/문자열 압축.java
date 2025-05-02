8d64e31b902692110c79a7989b011e035b7d3d3d
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-05-02 14:22:29 +0900

    [level 2] Title: 문자열 압축, Time: 3.52 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,45 +1,43 @@
+import java.util.*;
 class Solution {
+    // 문자열을 1개 ~ s/2까지 자르면서 판단 
     public int solution(String s) {
-        int answer = Integer.MAX_VALUE;
         
-        if (s.length() == 1) {
-            return 1;
-        }
-        for(int i = 1; i <= s.length() / 2; i++){
+        if (s.length() == 1) return 1;
+        
+        
+        int answer = Integer.MAX_VALUE;
+        int len = s.length();
+        
+        int cnt = 1;
+        for(int i = 1; i <= len/2; i++) {
+            // System.out.println(s.substring(0,i));
             StringBuilder sb = new StringBuilder();
-            int count = 1;
-            String base = s.substring(0,i);
-            for (int j = i; j <= s.length(); j+=i) {
-                
+            String base = s.substring(0, i); // 1 ~ n/2까지 앞에 부분
+            for(int j = i; j <= s.length(); j += i) {
                 String cur;
-                if (j+i > s.length()) {
-                    cur = s.substring(j);
-                } 
-                else {
-                    cur = s.substring(j,j+i);
+                if (j + i > s.length()) cur = s.substring(j);
+                else cur = s.substring(j, j + i);
+                
+                //System.out.println(cur);
+                
+                if (base.equals(cur)) { // 문자열이 같은경우
+                    cnt++;
                 }
-               
-                if (base.equals(cur)) {
-                    count++;
-                } 
+                
                 else {
-                    if (count > 1) {
-                        sb.append(count);
-                    }
-                    sb.append(base);
-                    base = cur; // base값 재정의
-                    count = 1;
+                    if (cnt > 1) sb.append(cnt).append(base);
+                    else sb.append(base);
+                    cnt = 1;
+                    base = cur;
                 }
-            }
-            
-            if (count > 1) {
-                sb.append(count);
+                
+                //sb.append(cur);
             }
             sb.append(base);
-            //System.out.println(sb.toString());
-            answer = Integer.min(answer, sb.length());
+            // System.out.println(sb);
+            answer = Math.min(answer, sb.length());
         }
-        
         return answer;
     }
 }
