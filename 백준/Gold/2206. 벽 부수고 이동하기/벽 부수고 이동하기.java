ec33bf8358697d97414f2934d42cd4f07fb1f1ad
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-05-16 17:59:21 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 780 ms, Memory: 147516 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -6,86 +6,88 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int [][] board;
-    static boolean [][][] visited;
+    static char[][] board;
+    static boolean[][][] visited;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        board = new int[n][m];
+        board = new char[n][m];
         visited = new boolean[n][m][2];
-
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             String input = br.readLine();
-            for(int j = 0; j < m; j++) {
-                board[i][j] = input.charAt(j) - '0';
+            for (int j = 0; j < m; j++) {
+                board[i][j] = input.charAt(j);
             }
         }
 
+        // (0,0) ~ (n,m)까지 이동, 최단거리
         Queue<Point> q = new LinkedList<>();
-        q.add(new Point(0,0,1,false));
+
+        // (0,0)에서 출발 및 방문처리
+        q.add(new Point(0, 0, 1, true));
+
+        // 0: 벽을 아직 부순 적 없음, 1: 벽을 부순 상태
         visited[0][0][0] = true;
 
         while (!q.isEmpty()) {
             Point cur = q.poll();
             int x = cur.x;
             int y = cur.y;
+            int dist = cur.dist;
+            boolean isPossible = cur.isPossible;
 
             if (x == n - 1 && y == m - 1) {
-                System.out.println(cur.dist);
+                System.out.println(dist);
                 return;
             }
 
-            boolean used = cur.used;
             for(int dir = 0; dir < 4; dir++) {
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
                 if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                int nxt = cur.dist + 1;
-
-                if (board[nx][ny] == 1) { // 벽을 만났을 때
-                    if (!used) { // 부순적이 없는 경우에 방문처리
-                        visited[nx][ny][1] = true;
-                        q.add(new Point(nx, ny, nxt, true));
-                    }
-                }
-
-                else if (board[nx][ny] == 0) { // 빈칸을 지나는경우
-                    if (!used && !visited[nx][ny][0]) { // 벽을 한번도 부순적이 없는 경우
-                        q.add(new Point(nx, ny, nxt, false));
+                if (board[nx][ny] == '0') {
+                    if (!visited[nx][ny][0] && isPossible) { // 벽을 부순 내역이 없는 경우
+                        q.add(new Point(nx, ny, dist + 1, true));
                         visited[nx][ny][0] = true;
                     }
 
-                    else if (used && !visited[nx][ny][1]) { // 벽을 부순적이 있는 경우
-                        q.add(new Point(nx, ny, nxt, true));
+                    else if (!visited[nx][ny][1] && !isPossible) { // 부순 경우
+                        q.add(new Point(nx, ny, dist + 1, false));
                         visited[nx][ny][1] = true;
                     }
+                }
+
+                else { // 벽인 경우
+                    if (!visited[nx][ny][0] && isPossible) {
+                        q.add(new Point(nx, ny, dist + 1, false));
+                        visited[nx][ny][0] = true; // 이제 방문한것으로 표시를 해줘야 함.
+                    }
+                    // visited[nx][ny][1]의 경우에는 체크하지 않는데, 어짜피 못지나가기 때문.
                 }
             }
         }
 
-//        for(int i = 0; i < n; i++) {
-//            for(int j = 0; j < m; j++) {
-//                System.out.print(dist[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-        System.out.println(-1);
+        if (!visited[n-1][m-1][0] || visited[n-1][m-1][1]) {
+            System.out.println(-1);
+        }
     }
 
-    public static class Point {
-        int x, y, dist;
-        boolean used;
-        public Point(int x, int y, int dist, boolean used){
+    static class Point {
+        int x;
+        int y;
+        int dist;
+        boolean isPossible;
+
+        public Point(int x, int y, int dist, boolean isPossible) {
             this.x = x;
             this.y = y;
             this.dist = dist;
-            this.used = used;
+            this.isPossible = isPossible;
         }
     }
 }
