f289a8c6ef779fc1fbdcd9b2ceb33de3cfc0fa60
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-06-03 02:03:28 +0900

    [Silver I] Title: 스타트와 링크, Time: 292 ms, Memory: 34280 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,23 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] arr;
+    static int[][] board;
     static int n;
-    static int [][] board;
-    static boolean [] visited;
-    static int res = Integer.MAX_VALUE;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-
-        // [1,2], [3,4]
-        // [1,3], [2,4]
-        // [1,4], [2,3]
-
         board = new int[n][n];
+
+        arr = new int[n/2];
         visited = new boolean[n];
+
         for(int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0; j < n; j++) {
@@ -25,37 +26,56 @@ public class Main {
             }
         }
 
-        dfs(0, 0);
-        System.out.println(res);
+        visited[0] = true;
+        dfs(1, 1);
+        System.out.println(min);
     }
 
-    public static void dfs(int idx, int cnt) {
-        if (cnt == n/2) {
-            int start = 0;
-            int link = 0;
+    public static void dfs(int depth, int nxt) {
+        if (depth == n/2) {
 
-            for(int i = 0; i < n - 1; i++) {
-                for(int j = i + 1; j < n; j++) {
-                    if (visited[i] && visited[j]) {
-                        start += board[i][j] + board[j][i];
-                    }
-
-                    if (!visited[i] && !visited[j]) {
-                        link += board[i][j] + board[j][i];
-                    }
-                }
+            List<Integer> start = new ArrayList<>();
+            List<Integer> link = new ArrayList<>();
+            for(int i = 0; i < n; i++) {
+                if (visited[i]) start.add(i);
+                else link.add(i);
             }
+            int start_score = getScore(start);
+            int link_score = getScore(link);
 
-            res = Math.min(res, Math.abs(start - link));
+//            for (int i = 0; i < start.size(); i++) {
+//                System.out.print(start.get(i) + " ");
+//            }
+//            System.out.println();
+//
+//            for(int j = 0; j < link.size(); j++) {
+//                System.out.print(link.get(j) + " ");
+//            }
+//            System.out.println();
+//            System.out.println(start_score + " " + link_score);
+            min = Math.min(min, Math.abs(start_score - link_score));
             return;
         }
 
-        for(int i = idx; i < n; i++) {
-            if (!visited[idx]) {
-                visited[idx] = true;
-                dfs(i + 1, cnt + 1);
-                visited[idx] = false;
+        for(int i = nxt; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i;
+                dfs(depth + 1, i + 1);
+                visited[i] = false;
             }
         }
+    }
+
+    public static int getScore(List<Integer> list) {
+        int score = 0;
+        for(int i = 0; i < list.size() - 1; i++) {
+            for(int j = i + 1; j < list.size(); j++) {
+                int a = list.get(i);
+                int b = list.get(j);
+                score += board[a][b] + board[b][a];
+            }
+        }
+        return score;
     }
 }
