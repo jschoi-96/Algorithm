a964055a06b364bc75b53f5a4e685d0946f109d4
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-06-17 23:28:08 +0900

    [level 2] Title: 지게차와 크레인, Time: 26.85 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -29,7 +29,6 @@ class Solution {
             else {
                 zigae(c);
             }
-            
         }
         
         // 결과 계산
@@ -45,88 +44,125 @@ class Solution {
     }
     
     public void crane(char c) {
+        System.out.println("crane(" + c + ") 실행 후:");
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if (board[i][j] == c) board[i][j] = '.';
             }
         }
+        print();
+    }
+    
+    public void setEdge(char c) {
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == c) {
+                board[i][0] = '.';
+            }
+            if (board[i][m - 1] == c) {
+                board[i][m - 1] = '.';
+            }
+        }
+        
+        for (int j = 0; j < m; j++) {
+            if (board[0][j] == c) {
+                board[0][j] = '.';
+            }
+            if (board[n - 1][j] == c) {
+                board[n-1][j] = '.';
+            }
+        }
     }
     
     public void zigae(char c) {
-        boolean[][] outside = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
+        // outside 배열 초기화
+        setEdge(c);
+        outside = new boolean[n][m];
         
-        // 1. 외부에서 부터 가장자리의 '.'을 모두 큐에 추가
-        for(int i = 0; i < n; i++) {
+        Queue<int[]> q = new LinkedList<>();
+        // 외부 가장자리 탐색 (빈 공간만)
+        for (int i = 0; i < n; i++) {
             if (board[i][0] == '.') {
                 q.add(new int[]{i, 0});
                 outside[i][0] = true;
             }
-            
-            if (board[i][m-1] == '.') {
-                q.add(new int[]{i, m-1});
+            if (board[i][m - 1] == '.') {
+                q.add(new int[]{i, m - 1});
                 outside[i][m-1] = true;
             }
         }
         
-        for(int i = 0; i < m; i++) {
-            if (board[0][i] == '.') {
-                q.add(new int[]{0, i});
-                outside[0][i] = true;
+        for (int j = 0; j < m; j++) {
+            if (board[0][j] == '.') {
+                q.add(new int[]{0, j});
+                outside[0][j] = true;
             }
-            
-            if (board[n-1][i] == '.') {
-                q.add(new int[]{n-1, i});
-                outside[n-1][i] = true;
+            if (board[n - 1][j] == '.') {
+                q.add(new int[]{n - 1, j});
+                outside[n-1][j] = true;
             }
         }
         
-        // 2. 큐에 있는 좌표들을 순회하며 다음 지점이 외부와 연결되는지 체크
+        // 외부에서 접근 가능한 빈 공간들 찾기
         while(!q.isEmpty()) {
             int[] cur = q.poll();
             int x = cur[0], y = cur[1];
+                        
             for(int dir = 0; dir < 4; dir++) {
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
+                
                 if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if (board[nx][ny] == '.' && !outside[nx][ny]) { // 컨테이너가 삭제되었고 외부와 연결되지 않았을 때
+                if (outside[nx][ny]) continue;
+                
+                // 빈 공간인 경우만 확산
+                if (board[nx][ny] == '.') {
                     q.add(new int[]{nx, ny});
                     outside[nx][ny] = true;
                 }
             }
         }
-                
-        // 3. 2차원 배열을 순회하며 외부의 노출된 컨테이너들을 제거해준다.
+        
+        // 외부 빈 공간과 인접한 해당 문자들 삭제
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                boolean flag = false;
-                
-                if (board[i][j] != c) continue;
-                for(int dir = 0; dir < 4; dir++) {
-                    int nx = i + dx[dir];
-                    int ny = j + dy[dir];
-                    
-                    // 다음 좌표가 범위를 벗어낫다는 뜻은 현재 좌표가 가장자리 (외부와 연결됨)
-                    if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-                        flag = true;
-                        break;
+                if (board[i][j] == c) {
+                    // 상하좌우 중 하나라도 외부 빈 공간과 연결되어 있으면 삭제
+                    boolean canDelete = false;
+                    for(int dir = 0; dir < 4; dir++) {
+                        int nx = i + dx[dir];
+                        int ny = j + dy[dir];
+                        
+                        if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                        if (outside[nx][ny]) {
+                            canDelete = true;
+                            break;
+                        }
                     }
-                    
-                    // 외부와 연결되어 있고, 컨테이너가 출고 된 경우 
-                    if (outside[nx][ny] && board[nx][ny] == '.') {
-                        flag = true;
-                        break;
+                    if (canDelete) {
+                        board[i][j] = '.';
                     }
                 }
-                if (flag) board[i][j] = '.';
             }
         }
+        
+        System.out.println("zigae(" + c + ") 실행 후:");
+        print();
     }
     
     public void print() {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    public void print2() {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                System.out.print(outside[i][j] + " ");
             }
             System.out.println();
         }
