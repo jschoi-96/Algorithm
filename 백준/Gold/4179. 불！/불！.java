4efe7f588977df7700c1abed74ac17fd9d545bd2
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-06-17 20:45:32 +0900

    [Gold III] Title: 불!, Time: 512 ms, Memory: 83116 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,81 +1,104 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int [] dx = {1,0,-1,0};
-    static int [] dy = {0,1,0,-1};
-
-    // J가 큐의 시작점 (입력에서 하나만 주어짐)
-    // 이동한 지점의 시간 값이 불이 이동한 시간보다 같거나 크다면, IMPOSSIBLE.
-    // 두개의 time 배열을 만들어서 시간 비교를 해야함.
-    // 탈출했을 때는,
+    static int r, c;
+    static char[][] board;
+    static int[][] fire;
+    static int[][] jihun;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        String [][] board = new String[r][c];
-        int [][] ji = new int[r][c];
-        int [][] fire = new int[r][c];
-        Queue<int[]> queue1 = new LinkedList<>();
-        Queue<int[]> queue2 = new LinkedList<>();
-        for(int i = 0; i < r; i++){
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        board = new char[r][c];
+        fire = new int[r][c];
+        jihun = new int[r][c];
+
+        Queue<int[]> f = new LinkedList<>();
+        boolean[][] fire_visited = new boolean[r][c];
+
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] jihun_visited = new boolean[r][c];
+
+        boolean hasFire = false;
+        for (int i = 0; i < r; i++) {
             String input = br.readLine();
-            for(int j = 0; j < c; j++) {
-                board[i][j] = String.valueOf(input.charAt(j));
-                ji[i][j] = -1; fire[i][j] = -1;
-                if (board[i][j].equals("J")) {
-                    ji[i][j] = 0;
-                    queue1.add(new int[]{i, j});
+            for (int j = 0; j < c; j++) {
+                board[i][j] = input.charAt(j);
+                if (board[i][j] == 'F') {
+                    f.add(new int[]{i, j});
+                    fire_visited[i][j] = true;
+                    hasFire = true;
                 }
-                else if (board[i][j].equals("F")) {
-                    fire[i][j] = 0;
-                    queue2.add(new int[]{i, j});
+
+                else if (board[i][j] == 'J') {
+                    q.add(new int[]{i, j});
+                    jihun_visited[i][j] = true;
                 }
             }
         }
 
-        // 불이 이동하는 시간
-        while(!queue2.isEmpty()) {
-            int [] cur = queue2.poll();
+        // 불을 먼저 이동시킨다.
+        while (!f.isEmpty()) {
+            int[] cur = f.poll();
             int x = cur[0];
             int y = cur[1];
             for(int dir = 0; dir < 4; dir++) {
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
-                if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-                if (board[nx][ny].equals("#") || fire[nx][ny] >= 0) continue;
+                if (nx < 0 || ny < 0 || nx >= r || ny >= c) continue;
+                if (fire_visited[nx][ny] || board[nx][ny] == '#') continue;
                 fire[nx][ny] = fire[x][y] + 1;
-                queue2.add(new int[]{nx, ny});
+                fire_visited[nx][ny] = true;
+                f.add(new int[]{nx, ny});
             }
         }
 
-        // 지훈이가 이동하는 시간
-        while (!queue1.isEmpty()) {
-            int[] cur = queue1.poll();
+        boolean flag = false;
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
+            if (x == 0 || y == 0 || x == r - 1 || y == c - 1) {
+                System.out.println(jihun[x][y] + 1);
+                flag = true;
+                break;
+            }
             for (int dir = 0; dir < 4; dir++) {
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
+                if (nx < 0 || ny < 0 || nx >= r || ny >= c) continue;
+                if (jihun_visited[nx][ny] || board[nx][ny] == '#' || board[nx][ny] == 'F') continue;
 
-                if (nx < 0 || nx >= r || ny < 0 || ny >= c) { // 범위를 벗어났다면, 탈출에 성공했다는 뜻
-                    System.out.println(ji[x][y] + 1);
-                    return;
+                if (!hasFire) {
+                    jihun[nx][ny] = jihun[x][y] + 1;
+                    jihun_visited[nx][ny] = true;
+                    q.add(new int[]{nx, ny});
                 }
-                if (ji[nx][ny] >= 0 || board[nx][ny].equals("#"))
-                    continue; // 방문했거나, 벽인 경우
-                if (fire[nx][ny] != -1 && fire[nx][ny] <= ji[x][y] + 1)
-                    continue; // 불이 먼저 도착하는 경우
-                ji[nx][ny] = ji[x][y] + 1;
-                queue1.add(new int[] { nx, ny });
+
+                if (fire[nx][ny] <= jihun[x][y] + 1) continue;
+                jihun[nx][ny] = jihun[x][y] + 1;
+                jihun_visited[nx][ny] = true;
+                q.add(new int[]{nx, ny});
             }
         }
-        System.out.println("IMPOSSIBLE");
+
+//        for (int i = 0; i < r; i++) {
+//            for (int j = 0; j < c; j++) {
+//                System.out.print(jihun[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        if (!flag) System.out.println("IMPOSSIBLE");
     }
 }
