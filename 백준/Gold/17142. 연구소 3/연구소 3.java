25c008ee1d009fef132b9d5ff71803d70b578e95
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-06-24 01:13:15 +0900

    [Gold III] Title: 연구소 3, Time: 312 ms, Memory: 45736 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -11,38 +11,44 @@ public class Main {
     static int n, m;
     static int[][] board;
     static int[][] time;
+    static boolean[] visited = new boolean[12]; // 10보타 살짝 크게
+    static List<int[]> virus = new ArrayList<>();
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int minTime = Integer.MAX_VALUE;
-    static List<int[]> virus = new ArrayList<>();
-    static int empty = 0;
-    static boolean[] check;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         board = new int[n][n];
         time = new int[n][n];
-        check = new boolean[12];
+        int emptyCnt = 0;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
-                if (board[i][j] == 1) time[i][j] = -1; // 벽일 때
-                else if (board[i][j] == 2) {
-                    time[i][j] = -2; // 바이러스
+                if (board[i][j] == 2) {
                     virus.add(new int[]{i, j});
+                    time[i][j] = -2; // 바이러스 시간 배열에 표기
                 }
-                else if (board[i][j] == 0) empty++;
+                else if (board[i][j] == 1) time[i][j] = -1; // 벽인 경우, 시간 배열에 표기
+                else if (board[i][j] == 0) emptyCnt++;
             }
         }
 
-        dfs(0,  0);
-        if (minTime == Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(minTime);
+        if (emptyCnt == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        dfs(0, 0);
+        if (minTime == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(minTime);
+        }
     }
 
     public static void dfs(int cur, int depth) {
@@ -52,13 +58,14 @@ public class Main {
         }
 
         for(int i = cur; i < virus.size(); i++) {
-            if (check[i]) continue;
-            int[] pos = virus.get(i);
-            check[i] = true;
-            time[pos[0]][pos[1]] = -3; // 활성 바이러스 표시
-            dfs(i + 1, depth + 1);
-            check[i] = false;
-            time[pos[0]][pos[1]] = -2; // 비활성으로 원복
+            if (!visited[i]) {
+                visited[i] = true;
+                int[] pos = virus.get(i);
+                time[pos[0]][pos[1]] = -3; // 바이러스를 활성화 시켜줌
+                dfs(i + 1, depth + 1);
+                time[pos[0]][pos[1]] = -2; // 다시 비활성화
+                visited[i] = false;
+            }
         }
     }
 
@@ -67,51 +74,43 @@ public class Main {
         boolean[][] visited = new boolean[n][n];
         Queue<int[]> q = new LinkedList<>();
 
-        int maxTime = 0;
-        int cnt = empty;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 if (copy[i][j] == -3) {
-                    copy[i][j] = 0;
-                    q.add(new int[] {i, j});
+                    q.add(new int[]{i, j});
                     visited[i][j] = true;
+                    copy[i][j] = 0; // 바이러스를 퍼트리는 시간 0으로 초기화
                 }
             }
         }
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
+            int x = cur[0], y = cur[1];
             for(int dir = 0; dir < 4; dir++) {
-                int nx = x + dx[dir];
-                int ny = y + dy[dir];
+                int nx = x + dx[dir], ny = y + dy[dir];
                 if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-                if (visited[nx][ny] || copy[nx][ny] == -1) continue;
+                if (visited[nx][ny] || board[nx][ny] == 1) continue;
 
-                if (board[nx][ny] == 0) { // 빈칸일 때
-                    cnt--;
-                    copy[nx][ny] = copy[x][y] + 1;
-                    maxTime = Math.max(maxTime, copy[nx][ny]);
-                }
-
-                else copy[nx][ny] = copy[x][y] + 1; // 바이러스를 지나갈 때 
-                q.add(new int[] {nx, ny});
+                copy[nx][ny] = copy[x][y] + 1;
                 visited[nx][ny] = true;
+                q.add(new int[]{nx, ny});
+
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if (board[i][j] == 0 && !visited[i][j]) {
-                    return;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) { // 바이러스 칸에 있는 시간 값은 고려 x
+                    if (!visited[i][j]) return;
+                    max = Math.max(max, copy[i][j]);
                 }
             }
         }
 
-
-        if (cnt == 0) minTime = Math.min(minTime, maxTime);
-        // print(copy);
+        minTime = Math.min(max, minTime);
     }
 
     public static int[][] copyTime() {
@@ -122,15 +121,5 @@ public class Main {
             }
         }
         return copy;
-    }
-
-    public static void print(int[][] copy) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(copy[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
     }
 }
