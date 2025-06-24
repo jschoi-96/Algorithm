bdbcb1a4d5b142c3786c010e5183f64cb476a02e
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-06-25 01:37:51 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.99 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -3,27 +3,25 @@ class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
         
-        PriorityQueue<int []> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0] , b[0]));
-        
         int currentServer = 0;
+        PriorityQueue<int[] > pq = new PriorityQueue<>((a,b) -> {
+           return a[1] - b[1]; // 종료시간이 빠른 순서대로 pop 
+        });
+        
         for(int i = 0; i < players.length; i++) {
-            if (players[i] < m) continue; // 사용자 m명 미만인 경우
+            
             int needServer = players[i] / m;
             
-            if (!pq.isEmpty() && pq.peek()[0] <= i) { // 
-                int [] cur = pq.poll();
-                currentServer -= cur[1];
+            if (!pq.isEmpty() && pq.peek()[1] == i) {
+                currentServer -= pq.poll()[0];
             }
             
             int addServer = needServer - currentServer;
-            if (currentServer < needServer) { // 서버 충원해야함.
-                pq.add(new int[]{i + k, addServer});
-                //System.out.println(needServer - currentServer);
+            if (currentServer < needServer) { // 서버를 추가 증설해야 하는 경우 
+                pq.add(new int[]{addServer, i + k});
                 currentServer += addServer;
                 answer += addServer;
             }
-
-            
         }
         return answer;
     }
