017e788b734d70f7e3c4323e010581799796a337
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-07-29 01:30:29 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,7 +1,6 @@
 -- 코드를 입력하세요
-SELECT o.animal_id, o.name
-from animal_ins i
-right join animal_outs o
-on i.animal_id = o.animal_id
-where i.animal_id is null
-order by o.animal_id
+SELECT O.ANIMAL_ID, O.NAME
+FROM ANIMAL_INS I
+RIGHT JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.DATETIME IS NULL
+ORDER BY ANIMAL_ID
