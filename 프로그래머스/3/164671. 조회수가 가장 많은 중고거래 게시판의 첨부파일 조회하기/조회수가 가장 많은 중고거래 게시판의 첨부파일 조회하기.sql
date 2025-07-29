6b1617a2d6618fa0ea01a7b29b83766b2a0aed43
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-07-29 13:20:23 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,9 +1,10 @@
 -- 코드를 입력하세요
 SELECT 
-    concat('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) as FILE_PATH
-from USED_GOODS_FILE
-where board_id = (select board_id
-                 from USED_GOODS_BOARD
-                 order by views desc
-                  limit 1)
-order by file_id desc
+    concat('/home/grep/src/', B.board_id, '/', file_id, file_name, file_ext) as FILE_PATH
+FROM USED_GOODS_BOARD B
+JOIN USED_GOODS_FILE F ON B.BOARD_ID = F.BOARD_ID
+WHERE B.BOARD_ID = (SELECT BOARD_ID
+                    FROM USED_GOODS_BOARD
+                    ORDER BY VIEWS DESC
+                    LIMIT 1)
+ORDER BY FILE_ID DESC
