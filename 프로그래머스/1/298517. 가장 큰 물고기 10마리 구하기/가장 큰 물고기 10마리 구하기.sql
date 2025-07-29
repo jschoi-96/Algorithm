841fe1705ff3d5387fd2c0e8375e26f651892a22
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-07-29 19:27:51 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,8 +1,6 @@
 -- 코드를 작성해주세요
-select 
-    id, IFNULL(LENGTH, 10) as LENGTH
-from
-    FISH_INFO
-order by
-    LENGTH desc, id 
-limit 10
+SELECT ID, LENGTH
+FROM FISH_INFO
+WHERE LENGTH >= 10
+ORDER BY LENGTH DESC, ID
+LIMIT 10
