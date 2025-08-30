d8ce4e7204070307c6a47d4b7efc2db517cc3540
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-08-30 15:28:11 +0900

    [Gold III] Title: 레이저 통신, Time: 140 ms, Memory: 16660 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.java
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.java
@@ -1,17 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int w, h;
     static char[][] board;
-    static int[] dx = {0, -1, 0, 1};
-    static int[] dy = {1, 0, -1, 0};
-    static int min_mirror = Integer.MAX_VALUE;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static int res = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,51 +29,51 @@ public class Main {
             }
         }
 
-        board[x][y] = '.'; // 시작점 초기화
-        Node start = new Node(x, y, -1, 0);
-        bfs(start);
+        board[x][y] = '.'; // 시작점은 초기화 해줌
+        bfs(x, y);
 
-        System.out.println(min_mirror);
+        System.out.println(res);
     }
 
-    public static void bfs (Node start) {
-        int[][][] visited = new int[h][w][4];
-        for(int i = 0; i < h; i++) {
-            for(int j = 0; j < w; j++) {
-                for(int d = 0; d < 4; d++) {
-                    visited[i][j][d] = Integer.MAX_VALUE;
+    public static void bfs(int x, int y) {
+        int[][][] visited = new int[h][w][4]; // 4가지 방향을 모두 체크하기 위해서 3차원 배열 선언ㅇ
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                for(int k = 0; k < 4; k++) {
+                    visited[i][j][k] = Integer.MAX_VALUE;
                 }
             }
         }
 
-        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.mirror - b.mirror);
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.mirror - b.mirror);
         for(int i = 0; i < 4; i++) {
-            visited[start.x][start.y][i] = 0;
-            pq.add(new Node(start.x, start.y, i, 0));
+            visited[x][y][i] = 0; // 시작점 초기화
+            pq.add(new Node(x, y, i, 0)); // 해당 지점에서 4가지 방향으로 가는 케이스 전부 고려
         }
 
         while(!pq.isEmpty()) {
             Node cur = pq.poll();
-            int x = cur.x;
-            int y = cur.y;
 
-            if (board[x][y] == 'C') { // 도착했을 때 거울의 최소 개수 갱신
-                min_mirror = Math.min(min_mirror, cur.mirror);
+            if (board[cur.x][cur.y] == 'C') {
+                res = Math.min(res, cur.mirror);
             }
 
-            if (visited[x][y][cur.dir] < cur.mirror) continue; // 최적경로가 아니기 때문에 스킵
+            // 방문하려는 곳이 현재 거울 개수보다 더 필요하다면 건너뜀
+            if (visited[cur.x][cur.y][cur.dir] < cur.mirror) continue;
 
             for(int dir = 0; dir < 4; dir++) {
-                int nx = x + dx[dir];
-                int ny = y + dy[dir];
-                if (nx < 0 || nx >= h || ny < 0 || ny >= w || board[nx][ny] == '*') continue;
+                int nx = cur.x + dx[dir];
+                int ny = cur.y + dy[dir];
 
-                int mirror_cnt = cur.mirror;
-                if (cur.dir != dir) mirror_cnt++;
+                if (nx < 0 || ny < 0 || nx >= h || ny >= w || board[nx][ny] == '*') continue;
+                int mirror = cur.mirror;
+                if (cur.dir != dir) { // 방향이 다른 경우, 거울이 있었다는 뜻 이므로 횟수 증가
+                    mirror++;
+                }
 
-                if (visited[nx][ny][dir] > mirror_cnt) {
-                    visited[nx][ny][dir] = mirror_cnt;
-                    pq.add(new Node(nx, ny, dir, mirror_cnt));
+                if (visited[nx][ny][dir] > mirror) {
+                    visited[nx][ny][dir] = mirror;
+                    pq.add(new Node(nx, ny, dir, mirror));
                 }
             }
         }
