c28f4fbfd2e71314f2415b9adb2e103040066a23
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-09-02 13:38:15 +0900

    [Gold IV] Title: 알고스팟, Time: 124 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
+    static int m, n;
     static int[][] board;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
@@ -16,8 +14,8 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        board = new int[n][m];
 
+        board = new int[n][m];
         for (int i = 0; i < n; i++) {
             String input = br.readLine();
             for (int j = 0; j < m; j++) {
@@ -29,45 +27,44 @@ public class Main {
     }
 
     public static void bfs() {
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.cnt - b.cnt);
-        pq.add(new Node(0, 0, 0));
-
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.cost - b.cost);
         int[][] dist = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 dist[i][j] = Integer.MAX_VALUE;
             }
         }
+
+        pq.add(new Node(0, 0, 0));
         dist[0][0] = 0;
 
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()) {
             Node cur = pq.poll();
             int x = cur.x;
             int y = cur.y;
-            int cnt = cur.cnt;
+            int cost = cur.cost;
 
             if (x == n - 1 && y == m - 1) {
-                System.out.println(cnt);
+                System.out.println(cost);
                 return;
             }
 
             for(int dir = 0; dir < 4; dir++) {
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
-
                 if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
 
-                if (board[nx][ny] == 1) {
-                    if (dist[nx][ny] > cnt + 1) {
-                        dist[nx][ny] = cnt + 1;
-                        pq.add(new Node(nx, ny, cnt + 1));
+                if (board[nx][ny] == 1) { // 벽인 경우
+                    if (dist[nx][ny] > cost + 1) { // 벽인 경우 부수는 cost가 1증가하기 때문에 cost+1과 비교
+                        dist[nx][ny] = cost + 1;
+                        pq.add(new Node(nx, ny, dist[nx][ny]));
                     }
                 }
 
                 else {
-                    if (dist[nx][ny] > cnt) {
-                        dist[nx][ny] = cnt;
-                        pq.add(new Node(nx, ny, cnt));
+                    if (dist[nx][ny] > cost) {
+                        dist[nx][ny] = cost;
+                        pq.add(new Node(nx, ny, dist[nx][ny]));
                     }
                 }
             }
@@ -77,11 +74,11 @@ public class Main {
     public static class Node {
         int x;
         int y;
-        int cnt;
-        public Node(int x, int y, int cnt) {
+        int cost;
+        public Node(int x, int y, int cost) {
             this.x = x;
             this.y = y;
-            this.cnt = cnt;
+            this.cost = cost;
         }
     }
 }
