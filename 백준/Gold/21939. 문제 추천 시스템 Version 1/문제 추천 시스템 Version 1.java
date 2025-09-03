13bbe3bc282cf4ec2ec645ed26d622b04a46e8e2
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-09-04 02:13:38 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 500 ms, Memory: 55764 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -13,77 +11,80 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        PriorityQueue<int[]> pos = new PriorityQueue<>((a, b) -> {
-            if (a[1] == b[1]) {
-                return b[0] - a[0];
-            };
-            return b[1] - a[1];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        PriorityQueue<Node> min_pq = new PriorityQueue<>((a, b) -> {
+            if (a.diff == b.diff) return a.num - b.num;
+            return a.diff - b.diff;
         });
 
-        Map<Integer, Integer> map = new HashMap<>(); // 추천 문제 리스트를 저장함
-
-        PriorityQueue<int[]> neg = new PriorityQueue<>((a, b) -> {
-            if (a[1] == b[1]) {
-                return a[0] - b[0];
-            }
-            return a[1] - b[1];
+        PriorityQueue<Node> max_pq = new PriorityQueue<>((a, b) -> {
+            if (a.diff == b.diff) return b.num - a.num;
+            return b.diff - a.diff;
         });
 
         for(int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int idx = Integer.parseInt(st.nextToken());
-            int diff = Integer.parseInt(st.nextToken());
-            pos.add(new int[]{idx, diff});
-            neg.add(new int[]{idx, diff});
-            map.put(idx, diff);
+            int p = Integer.parseInt(st.nextToken()); // 문저번호
+            int q = Integer.parseInt(st.nextToken()); // 난이도
+            map.put(p, q);
+            max_pq.add(new Node(p, q));
+            min_pq.add(new Node(p, q));
         }
 
         int m = Integer.parseInt(br.readLine());
-
         StringBuilder sb = new StringBuilder();
+
         for(int i = 0; i < m; i++) {
             String input = br.readLine();
+            String[] arr = input.split(" ");
 
-            String[] s = input.split(" ");
-            if (s[0].equals("add")) {
-                int idx = Integer.parseInt(s[1]);
-                int diff = Integer.parseInt(s[2]);
-
-                // if (list.contains(idx)) continue; //
-                pos.add(new int[]{idx, diff});
-                neg.add(new int[]{idx, diff});
-                map.put(idx, diff);
+            if (arr[0].equals("add")) {
+                Integer p = Integer.parseInt(arr[1]);
+                Integer q = Integer.parseInt(arr[2]);
+                map.put(p, q);
+                max_pq.add(new Node(p, q));
+                min_pq.add(new Node(p, q));
             }
 
-            else if (s[0].equals("solved")) {
-                int idx = Integer.parseInt(s[1]);
-                map.remove(idx);
+            else if (arr[0].equals("solved")) {
+                map.remove(Integer.parseInt(arr[1]));
             }
 
-            else {
-                int idx = Integer.parseInt(s[1]);
-                if (idx == 1) {
-                    while (true) {
-                        // 맵에서
-                        if (map.containsKey(pos.peek()[0]) && map.get(pos.peek()[0]) == pos.peek()[1]) {
-                            sb.append(pos.peek()[0]).append("\n");
+            else { // recommend
+                int x = Integer.parseInt(arr[1]);
+                if (x == 1) {
+                    while(!max_pq.isEmpty()) {
+                        Node cur = max_pq.peek();
+                        if (map.containsKey(cur.num) && map.get(cur.num) == cur.diff) {
+                            sb.append(max_pq.peek().num).append("\n");
                             break;
                         }
-                        pos.poll();
+                        max_pq.poll();
                     }
                 }
 
                 else {
-                    while (true) {
-                        if (map.containsKey(neg.peek()[0]) && map.get(neg.peek()[0]) == neg.peek()[1]) {
-                            sb.append(neg.peek()[0]).append("\n");
+                    while (!min_pq.isEmpty()) {
+                        Node cur = min_pq.peek();
+                        if (map.containsKey(cur.num) && map.get(cur.num) == cur.diff) {
+                            sb.append(min_pq.peek().num).append("\n");
                             break;
                         }
-                        neg.poll();
+                        min_pq.poll();
                     }
                 }
             }
         }
         System.out.println(sb);
+    }
+
+    public static class Node {
+        int num;
+        int diff;
+        public Node(int num, int diff) {
+            this.num = num;
+            this.diff = diff;
+        }
     }
 }
