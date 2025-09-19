223b71b2898f5d8f7d97fb513e7f27e9f26a6072
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2025-09-19 20:18:48 +0900

    [Gold IV] Title: 여행 가자, Time: 160 ms, Memory: 18364 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -6,36 +6,25 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int [] parent;
-
+    static int[] parent;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-        parent = new int[n + 1];
+        parent = new int[n+1];
         for(int i = 1; i <= n; i++) {
             parent[i] = i;
         }
-
-//        for(int i = 1; i <= n; i++) {
-//            System.out.print(parent[i] + " ");
-//        }
         for(int i = 1; i <= n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 1; j <= n; j++) {
-                int k = Integer.parseInt(st.nextToken());
-                if (k == 1) { //
+                int num = Integer.parseInt(st.nextToken());
+                if (num == 1) {
                     union(i, j);
                 }
             }
         }
-
-//        System.out.println();
-//        for(int i = 1; i <= n; i++) {
-//            System.out.print(parent[i] + " ");
-//        }
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
@@ -49,18 +38,16 @@ public class Main {
         System.out.println("YES");
     }
 
-    public static void union(int i, int j) {
-        i = find(i);
-        j = find(j);
-        if (i != j) {
-            parent[j] = i;
-        }
+    public static void union(int x, int y) {
+        int a = find(x);
+        int b = find(y);
+        if (a != b) parent[b] = a;
     }
 
-    public static int find(int num) {
-        if (num == parent[num]) {
-            return num;
+    public static int find(int k) {
+        if (parent[k] != k) {
+            parent[k] = find(parent[k]);
         }
-        return parent[num] = find(parent[num]);
+        return parent[k];
     }
 }
